6f8708ce12e97ea0034e47555ad297293c758b05
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-07 11:19:17 -0600

    Passing

--- Leetcode/median_of_two_sorted_arrays/median_of_two_sorted_arrays.rb
+++ Leetcode/median_of_two_sorted_arrays/median_of_two_sorted_arrays.rb
@@ -2,8 +2,28 @@
 # @param {Integer[]} nums2
 # @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
-  merged_array = (nums1 + nums2)
-  (merged_array.sum.to_f / merged_array.count.to_f)
+  removed = 0
+  while(nums1.length > 0 || nums2.length > 0)
+      current = if nums2.length == 0
+          nums1.shift
+      elsif nums1.length == 0
+          nums2.shift
+      elsif nums1.first <= nums2.first
+          nums1.shift
+      else
+          nums2.shift
+      end
+
+    removed +=1
+    remaining = nums1.length + nums2.length
+
+    if removed == remaining
+      last = current
+    elsif removed > remaining
+      last ||= current
+      return (current+last) /2.0
+    end
+  end
 end
 
 
