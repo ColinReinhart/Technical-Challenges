d80a3d472a7be076f933c5f1c2413ecd2da90ffb
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-11 10:29:25 -0600

    completed before test

--- Leetcode/group_the_people_given_the_group_size_they_belong_to/group_the_people.rb
+++ Leetcode/group_the_people_given_the_group_size_they_belong_to/group_the_people.rb
@@ -1,7 +1,22 @@
 # @param {Integer[]} group_sizes
 # @return {Integer[][]}
 def group_the_people(group_sizes)
-  require 'pry'; binding.pry
+  output = []
+  temp_group = []
+  group_sizes.each_with_index do |s, i|
+    if s == 1
+      output << [i]
+    elsif temp_group.size < s
+      temp_group << i
+      if temp_group.size == s
+        output << temp_group
+      end
+    elsif temp_group.size == s
+      temp_group = [i]
+    else temp_group.size > s
+    end
+  end
+  return output
 end
 
 p group_the_people([3,3,3,3,3,1,3])
