e7ff2bf7785d4222faf0a8564e9af3929ea3cf48
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-11 09:25:45 -0600

    Complete

---
+++ Leetcode/pascals_triangle/pascals_triangle.rb
@@ -0,0 +1,27 @@
+# @param {Integer} num_rows
+# @return {Integer[][]}
+def generate(num_rows)
+ triangle = []
+    num_rows.times do |i|
+        case i
+        when 0 then row = [1]    # base case first row
+        when 1 then row = [1, 1] # base case second row
+        else
+            # Start the new row
+            row = [1]
+            # Collect the middle of the new row using a sliding
+            # window of two that sums pairs of the previous row
+            prev_row = triangle[-1]
+            prev_row.each_cons(2) do |(i ,j)|
+                row << i + j
+            end
+            # Finish the row
+            row << 1
+        end
+        triangle << row
+    end
+    triangle
+end
+
+p generate(5)
+p generate(1)
