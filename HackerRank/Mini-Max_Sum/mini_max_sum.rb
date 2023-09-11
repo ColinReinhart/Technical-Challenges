9beeb51c2fafaf15f728b1dd610c62c6aec5f851
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-11 15:36:47 -0600

    Hacker Plus Minus Setup

---
+++ HackerRank/Mini-Max_Sum/mini_max_sum.rb
@@ -0,0 +1,16 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr)
+    require 'pry'; binding.pry
+end
+
+miniMaxSum([1, 3, 5, 7, 9])
