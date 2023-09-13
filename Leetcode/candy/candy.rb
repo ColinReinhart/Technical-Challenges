df9600da599e2db7e868a1315c0e80959b014296
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-13 10:04:45 -0600

    Passing base examples, failing example 7

--- Leetcode/candy/candy.rb
+++ Leetcode/candy/candy.rb
@@ -1,8 +1,26 @@
 # @param {Integer[]} ratings
 # @return {Integer}
 def candy(ratings)
-  require 'pry'; binding.pry
+  pieces = ratings.count
+  ratings.each_with_index do |c, i|
+    if i == 0
+      if c > ratings[i+1]
+         pieces += 1
+      end
+    elsif i == ratings.count-1
+      if c > ratings[i-1]
+        pieces += 1
+      end
+    else
+      if c > ratings[i+1] || c > ratings[i-1]
+        pieces +=1
+      end
+    end
+  end
+  pieces
 end
 
-p candy([1, 0, 2])
+# p candy([1, 0, 2])
 # p candy([1, 2, 2])
+# p candy([1,3,2,2,1])
+p candy([1,2,87,87,87,2,1])
