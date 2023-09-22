5780dd4c557afae1c0b50b768a69b0f286aa925a
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-22 15:30:32 -0600

    Pair Count Passing All Casses

--- HackerRank/Sales_By_Match/sales_by_match.rb
+++ HackerRank/Sales_By_Match/sales_by_match.rb
@@ -1,5 +1,11 @@
 def sockMerchant(n, ar)
-    require 'pry'; binding.pry
+  hash = Hash.new(0)
+  pair_count = 0
+  ar.map { |x| hash[x] += 1 }
+  hash.each do |x|
+    pair_count += x[1]/2
+  end
+  pair_count
 end
 
 p sockMerchant(7, [1,2,1,2,1,3,2])
