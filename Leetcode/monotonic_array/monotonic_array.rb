af80476ac84bf94873b566a5efd868e56a804ee7
Colin Reinhart <colinbreinhart@gmail.com>
2023-09-29 12:41:20 -0600

    Setup monotonic array

---
+++ Leetcode/monotonic_array/monotonic_array.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+def is_monotonic(nums)
+  require 'pry'; binding.pry
+end
+
+p is_monotonic([1,2,2,3])
+p is_monotonic([6,5,4,4])
+p is_monotonic([1,3,2])
