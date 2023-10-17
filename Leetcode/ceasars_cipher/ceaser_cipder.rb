b203ab0b40a32b6d7f68b93d2fc19da1bd1ca771
Colin Reinhart <colinbreinhart@gmail.com>
2023-10-17 14:40:09 -0600

    Add ceasars cipher to README

--- Leetcode/ceasars_cipher/ceaser_cipder.rb
+++
@@ -1,5 +0,0 @@
-def caesarCipher(s, k)
-    require 'pry'; binding.pry
-end
-
-p caesarCipher("There's-a-starman-waiting-in-the-sky", 3)
--- Leetcode/minimum_number_of_operations_to_make_array_continuous/min_operations.rb
+++ Leetcode/minimum_number_of_operations_to_make_array_continuous/min_operations.rb
@@ -1,7 +1,24 @@
 def min_operations(nums)
-  require 'pry'; binding.pry
+  operations = 0
+  while nums != nums.uniq || nums.length - 1 != nums.max - nums.min do
+    if nums == nums.uniq && nums.length - 1 == nums.max - nums.min
+      operations
+    elsif nums != nums.uniq
+      nums = nums.uniq
+      operations += 1
+    elsif nums.length - 1 < nums.max - nums.min
+      x = (nums.max - nums.min) - (nums.length - 1)
+      y = nums.max - x
+      nums[-1] = y
+      operations += 1
+    end
+  end
+  operations
 end
 
 
-p min_operations([4,2,5,3])
-# p min_operations([1,2,3,5,6])
+# p min_operations([4,2,5,3])
+p min_operations([1,2,3,5,6])
+# p min_operations([1,1,2,3,5,6])
+# p min_operations([1, 5, 7, 10])
+# p min_operations([1,10,100,1000])
