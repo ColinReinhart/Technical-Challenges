a6342b54837cac7dad0ef62d756d29c8f835998e
Colin Reinhart <colinbreinhart@gmail.com>
2023-12-06 11:19:26 -0700

    Passing all casses

--- Leetcode/calculate_money_in_leetcode_bank/calculate_money.rb
+++ Leetcode/calculate_money_in_leetcode_bank/calculate_money.rb
@@ -1,7 +1,13 @@
 def total_money(n)
-  require 'pry'; binding.pry
+  weeks = n / 7
+  days = n % 7
+  total = 0
+  total += 28 * weeks
+  total += (weeks * (weeks - 1)) / 2 * 7
+  total += (weeks + 1..weeks + days).to_a.sum
+  total
 end
 
 p total_money(4)
-# p total_money(10)
-# p total_money(20)
+p total_money(10)
+p total_money(20)
