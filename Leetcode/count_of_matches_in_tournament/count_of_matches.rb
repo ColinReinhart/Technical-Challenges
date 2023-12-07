36b3edb129b4452e5d20b230979c9aafe1ab45e9
Colin Reinhart <colinbreinhart@gmail.com>
2023-12-07 15:53:51 -0700

    Passing

--- Leetcode/count_of_matches_in_tournament/count_of_matches.rb
+++ Leetcode/count_of_matches_in_tournament/count_of_matches.rb
@@ -1,7 +1,16 @@
 def number_of_matches(n)
   matches = 0
-  require 'pry'; binding.pry
+  while n > 1
+    if n.even?
+      matches += n / 2
+      n = n / 2
+    else
+      matches += (n - 1) / 2
+      n = (n - 1) / 2 + 1
+    end
+  end
+  matches
 end
 
 p number_of_matches(7)
-# p number_of_matches(14)
+p number_of_matches(14)
