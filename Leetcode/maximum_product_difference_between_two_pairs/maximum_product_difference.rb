d9f96733d407d235220d09035d948102e0861e3b
Colin Reinhart <colinbreinhart@gmail.com>
2024-01-04 15:28:25 -0700

    Update README to include Mimimum Number of Operations to Make Array Empty

--- Leetcode/maximum_product_difference_between_two_pairs/maximum_product_difference.rb
+++ Leetcode/maximum_product_difference_between_two_pairs/maximum_product_difference.rb
@@ -1,6 +1,6 @@
 def max_product_difference(nums)
-  require 'pry'; binding.pry
+  (nums.sort[-1] * nums.sort[-2]) - (nums.sort[0] * nums.sort[1])
 end
 
-P max_product_difference([5,6,2,7,4])
-P max_product_difference([4,2,5,9,7,4,8])
+p max_product_difference([5,6,2,7,4])
+p max_product_difference([4,2,5,9,7,4,8])
