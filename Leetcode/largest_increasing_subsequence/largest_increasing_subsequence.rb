5b61c018ed8be993e7e5b83d7ba68da86ce26302
Colin Reinhart <colinbreinhart@gmail.com>
2024-01-05 13:09:52 -0700

    Largets Increasing Subsequence setup

---
+++ Leetcode/largest_increasing_subsequence/largest_increasing_subsequence.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def length_of_lis(nums)
+  require 'pry'; binding.pry
+end
+
+p length_of_lis([10,9,2,5,3,7,101,18])
+# p length_of_lis([0,1,0,3,2,3])
+# p length_of_lis([7,7,7,7,7,7,7])
