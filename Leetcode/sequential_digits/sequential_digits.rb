cf14386e191fdf60d7b9f13cd685b2e043460c22
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-02 14:18:43 -0700

    Refactored faster solution, first to beat %100 of submissions!

--- Leetcode/sequential_digits/sequential_digits.rb
+++ Leetcode/sequential_digits/sequential_digits.rb
@@ -2,33 +2,18 @@
 # @param {Integer} high
 # @return {Integer[]}
 def sequential_digits(low, high)
-  list = []
-  array = (low..high).to_a
-  temp = []
-  array.each do |num|
-    split = num.to_s.split('')
-    split_i = split.map { |x| x.to_i }
-    split_i.each_with_index do |n, i|
-      if i == split.count - 1
-        if n.to_i == temp[-1] + 1
-          temp << n.to_i
-          list << temp.join.to_i
-          temp = []
-        elsif n.to_i != temp[-1] + 1
-          temp = []
-          break
-        end
-      elsif temp == []
-        temp << n.to_i
-      elsif n.to_i == temp[-1] + 1
-        temp << n.to_i
-      else n.to_i != temp[-1] + 1
-        temp = []
-        break
-      end
+  result = []
+  max_length = high.to_s.length
+
+  (1..max_length).each do |length|
+    (1..(10 - length)).each do |start|
+      num = start
+      (length - 1).times { num = num * 10 + (num % 10 + 1) }
+      result << num if num >= low && num <= high
     end
   end
-  return list
+
+  result.sort
 end
 
 
