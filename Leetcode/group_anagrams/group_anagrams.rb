7719e732da68438edb356e3e5393172b628fa908
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-06 14:48:22 -0700

    Passing all casses

--- Leetcode/group_anagrams/group_anagrams.rb
+++ Leetcode/group_anagrams/group_anagrams.rb
@@ -1,7 +1,15 @@
 # @param {String[]} strs
 # @return {String[][]}
 def group_anagrams(strs)
-  require 'pry'; binding.pry
+  hash = Hash.new { |h, k| h[k] = [] }
+
+  strs.each do |str|
+    sorted_str = str.chars.sort.join
+    hash[sorted_str] << str
+  end
+  hash.values
 end
 
 p group_anagrams(["eat","tea","tan","ate","nat","bat"])
+p group_anagrams([""])
+p group_anagrams(["a"])
