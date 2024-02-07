4ba5c1c57f0f7d93b1b820d9226d5c8781e60375
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-07 14:41:41 -0700

    Passing all cases

--- Leetcode/sort_characters_by_frequency/sort_characters.rb
+++ Leetcode/sort_characters_by_frequency/sort_characters.rb
@@ -1,9 +1,14 @@
 # @param {String} s
 # @return {String}
 def frequency_sort(s)
-  require 'pry'; binding.pry
+  wip = []
+  split = s.split('')
+  hash = Hash.new(0)
+  split.each { |char| hash[char] += 1 }
+  hash.sort_by { |k,v| -v }.each { |k, v| v.times { wip << k }}
+  wip.join
 end
 
 p frequency_sort("tree")
-# p frequency_sort("cccaaa")
-# p frequency_sort("Aabb")
+p frequency_sort("cccaaa")
+p frequency_sort("Aabb")
