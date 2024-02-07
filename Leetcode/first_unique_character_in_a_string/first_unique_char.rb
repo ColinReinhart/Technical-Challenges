aabeef63f06fb220e34b4995d6d8667717b272c6
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-07 09:48:01 -0700

    Passing all casses, but ugly, needs refactor

--- Leetcode/first_unique_character_in_a_string/first_unique_char.rb
+++ Leetcode/first_unique_character_in_a_string/first_unique_char.rb
@@ -1,7 +1,18 @@
 # @param {String} s
 # @return {Integer}
 def first_uniq_char(s)
-  require 'pry'; binding.pry
+  first_unique = false
+  split = s.split('')
+  split.each_with_index do |char, indx|
+    if split[indx+1..-1].include?(char) || split[0...indx].include?(char)
+
+    else
+      return indx
+    end
+  end
+  return -1
 end
 
 p first_uniq_char("leetcode")
+p first_uniq_char("loveleetcode")
+p first_uniq_char("aabb")
