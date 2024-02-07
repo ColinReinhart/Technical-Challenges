866364cd12eca06be6422eb174330c22026a27c6
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-07 10:04:05 -0700

    Minor refactor to clean up code

--- Leetcode/first_unique_character_in_a_string/first_unique_char.rb
+++ Leetcode/first_unique_character_in_a_string/first_unique_char.rb
@@ -1,14 +1,9 @@
 # @param {String} s
 # @return {Integer}
 def first_uniq_char(s)
-  first_unique = false
   split = s.split('')
   split.each_with_index do |char, indx|
-    if split[indx+1..-1].include?(char) || split[0...indx].include?(char)
-
-    else
-      return indx
-    end
+    return indx unless split[indx+1..-1].include?(char) || split[0...indx].include?(char)
   end
   return -1
 end
