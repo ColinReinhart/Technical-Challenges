3c42a96e931855d8dea656251cddd733617ebf84
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-08 11:07:17 -0700

    Working but taking FOREVER

--- Leetcode/perfect_squares/perfect_squares.rb
+++ Leetcode/perfect_squares/perfect_squares.rb
@@ -1,16 +1,36 @@
 # @param {Integer} n
 # @return {Integer}
 def num_squares(n)
-  count = 0
-  shortest_sum = []
+  results = []
   squares = (1..11).map { |x| x * x }
   less_than = []
-  squares.each { |x| less_than << x if x < n }
-  def find_combinations(arr, num)
-    require 'pry'; binding.pry
+  squares.each { |x| less_than << x if x <= n }
+  def find_combinations(arr, target, results, index, current_combination)
+    if target == 0
+      results << current_combination.dup
+      return
+    end
+
+    return if target < 0 || index >= arr.length
+
+    current_combination << arr[index]
+    find_combinations(arr, target - arr[index], results, index, current_combination)
+
+    find_combinations(arr, target - arr[index], results, index, current_combination)
+
+    current_combination.pop
+    find_combinations(arr, target, results, index + 1, current_combination)
+
   end
-  find_combinations(less_than, n)
+  find_combinations(less_than, n, results, 0, [])
+  unique = results.uniq
+  sorted = unique.sort_by { |array| array.length }
+  # require 'pry'; binding.pry
+  sorted.shift.count
 end
 
 p num_squares(12)
-# p num_squares(13)
+p num_squares(13)
+p num_squares(17)
+p num_squares(1)
+p num_squares(20)
