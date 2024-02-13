884a99cc646469264264e7fae8dbfa9a207ee89f
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-13 11:39:51 -0700

    Setup First Palindromic String;

---
+++ Leetcode/first_palindromic_string/first_palindromic.rb
@@ -0,0 +1,7 @@
+def first_palindrome(words)
+
+end
+
+p first_palindrome(["abc","car","ada","racecar","cool"])
+# p first_palindrome(["notapalindrome","racecar"])
+# p first_palindrome(["def","ghi"])
