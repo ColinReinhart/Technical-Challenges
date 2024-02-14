d830053e4c84fb0b793f2d3ea32f81a9c6001de9
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-14 10:41:28 -0700

    Passing all cases, but very slow and clunky

--- Leetcode/rearrange_array_elements_by_sign/rearrange_elements.rb
+++ Leetcode/rearrange_array_elements_by_sign/rearrange_elements.rb
@@ -1,6 +1,25 @@
 def rearrange_array(nums)
-  require 'pry'; binding.pry
+  neg = []
+  pos = []
+  wip = []
+  nums.each do |num|
+    if num.negative?
+      neg << num
+    else
+      pos << num
+    end
+  end
+  def combine(neg, pos, wip)
+    if neg == [] || pos == []
+      return wip
+    else
+      wip << pos.shift
+      wip << neg.shift
+      combine(neg, pos, wip)
+    end
+  end
+  combine(neg, pos, wip)
 end
 
 p rearrange_array([3,1,-2,-5,2,-4])
-# p rearrange_array([-1,1])
+p rearrange_array([-1,1])
