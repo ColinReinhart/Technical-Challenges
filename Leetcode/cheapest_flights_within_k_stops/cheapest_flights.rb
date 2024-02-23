3dd785b9b74ad5096b41449cbe14c5ece147126c
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-23 14:17:34 -0700

    Passing all cases

--- Leetcode/cheapest_flights_within_k_stops/cheapest_flights.rb
+++ Leetcode/cheapest_flights_within_k_stops/cheapest_flights.rb
@@ -5,7 +5,30 @@
 # @param {Integer} k
 # @return {Integer}
 def find_cheapest_price(n, flights, src, dst, k)
-  require 'pry'; binding.pry
+  neighbors = Hash.new{|h, k| h[k] = []}
+
+  flights.each do|from, to, price|
+    neighbors[from] << [price, to]
+  end
+
+  queue = neighbors[src].map do |price, to|
+    [price, to, k]
+  end.sort
+
+  visited = {src => k}
+
+  until queue.empty?
+    price, city, remaining = queue.shift
+    visited[city] = remaining
+    return price if city == dst
+    neighbors[city].each do |cost, to|
+        next if visited[to] && visited[to] >= remaining
+        next_price = price + cost
+        index = queue.bsearch_index {|price, _, _| price > next_price} || queue.size
+        queue.insert(index, [next_price, to, remaining-1])
+    end if remaining > 0
+  end
+  -1
 end
 
 p find_cheapest_price(4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1)
