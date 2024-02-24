6c401cdba09d600c4df7444d1b38625dbc5ff80c
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-24 13:44:45 -0700

    Passing base cases, issue with simultanious secret sharing

--- Leetcode/find_all_people_with_secret/find_all_people.rb
+++ Leetcode/find_all_people_with_secret/find_all_people.rb
@@ -1,7 +1,17 @@
 def find_all_people(n, meetings, first_person)
-  require 'pry'; binding.pry
+  confedantes = [0, first_person]
+  sorted = meetings.sort_by { |a, b, t| t }
+  sorted.each do |m|
+    if confedantes.include?(m[0])
+      confedantes << m[1]
+    elsif confedantes.include?(m[1])
+      confedantes << m[0]
+    end
+  end
+  confedantes.uniq
 end
 
-find_all_people(6, [[1,2,5],[2,3,8],[1,5,10]], 1)
-# find_all_people(4, [[3,1,3],[1,2,2],[0,3,3]], 3)
-# find_all_people(5, [[3,4,2],[1,2,1],[2,3,1]], 1)
+p find_all_people(6, [[1,2,5],[2,3,8],[1,5,10]], 1)
+p find_all_people(4, [[3,1,3],[1,2,2],[0,3,3]], 3)
+p find_all_people(5, [[3,4,2],[1,2,1],[2,3,1]], 1)
+p find_all_people(11,[[5,1,4],[0,4,18]] , 1)
