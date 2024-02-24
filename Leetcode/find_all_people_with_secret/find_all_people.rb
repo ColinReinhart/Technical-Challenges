268856050d1add72f1dd618a77613f8d7f591d4a
Colin Reinhart <colinbreinhart@gmail.com>
2024-02-24 13:12:10 -0700

    Setup Find All People With Secret

---
+++ Leetcode/find_all_people_with_secret/find_all_people.rb
@@ -0,0 +1,7 @@
+def find_all_people(n, meetings, first_person)
+  require 'pry'; binding.pry
+end
+
+find_all_people(6, [[1,2,5],[2,3,8],[1,5,10]], 1)
+# find_all_people(4, [[3,1,3],[1,2,2],[0,3,3]], 3)
+# find_all_people(5, [[3,4,2],[1,2,1],[2,3,1]], 1)
