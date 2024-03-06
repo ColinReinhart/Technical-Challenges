085009e62d511162fc3ed08d276068e9244e6cd5
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-06 11:04:51 -0700

    Add icons

--- Leetcode/find_all_people_with_secret/find_all_people.rb
+++ Leetcode/find_all_people_with_secret/find_all_people.rb
@@ -1,16 +1,26 @@
 def find_all_people(n, meetings, first_person)
-  confidantes = [0, first_person]
+  confidantes = [0, first_person]  # Initially, both person 0 and first_person have the secret
+  meeting_attendees = Hash.new { |hash, key| hash[key] = [] }  # Keep track of meetings attended by each person
 
-  sorted_meetings = meetings.sort_by { |m| m[2] }
+  sorted_meetings = meetings.sort_by { |m| m[2] }  # Sort meetings by time
 
   sorted_meetings.each do |meeting|
     xi, yi, time = meeting
+
+    # Update meeting attendees for both xi and yi
+    meeting_attendees[xi] << meeting
+    meeting_attendees[yi] << meeting
+
+    # Check if either person xi or yi already knows the secret
     if confidantes.include?(xi) || confidantes.include?(yi)
-      confidantes |= [xi, yi]
+      # If one person from the meeting already knows the secret, spread the secret to all attendees of that meeting
+      attendees = [xi, yi]
+      attendees.concat(meeting_attendees[xi].map { |m| m[0] } + meeting_attendees[yi].map { |m| m[1] })
+      confidantes |= attendees
     end
   end
 
-  (0...n).to_a & confidantes.sort
+  (0...n).to_a & confidantes.sort  # Include both person 0 and first_person, and sort the result
 end
 
 p find_all_people(6, [[1,2,5],[2,3,8],[1,5,10]], 1)
