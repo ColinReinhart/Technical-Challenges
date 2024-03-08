e263dcac0f95680a1e1a7ab15196f9bbe47b490a
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-08 12:59:21 -0700

    Count Elements with Maximum Frequency setup

---
+++ Leetcode/count_elements_with_maximum_frequency/count_elelments.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def max_frequency_elements(nums)
+  require 'pry'; binding.pry
+end
+
+p max_frequency_elements([1,2,2,3,1,4])
+# p max_frequency_elements([1,2,3,4,5])
