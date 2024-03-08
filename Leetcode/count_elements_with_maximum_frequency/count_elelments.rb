d3d4c835029d7c2603a26bf0d721ae88438e732b
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-08 13:10:55 -0700

    Passing all ceses

--- Leetcode/count_elements_with_maximum_frequency/count_elelments.rb
+++ Leetcode/count_elements_with_maximum_frequency/count_elelments.rb
@@ -1,8 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_frequency_elements(nums)
-  require 'pry'; binding.pry
+  count = 0
+  hash = Hash.new(0)
+  nums.each { |x| hash[x] += 1 }
+  max = hash.values.max
+  count += hash.values.sum { |v| v == max ? max : 0 }
+  count
 end
 
 p max_frequency_elements([1,2,2,3,1,4])
-# p max_frequency_elements([1,2,3,4,5])
+p max_frequency_elements([1,2,3,4,5])
