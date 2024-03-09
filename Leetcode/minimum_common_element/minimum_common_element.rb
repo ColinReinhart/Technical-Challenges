ff714506813ca184e45646c3d5fce8ac0214c80b
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-09 13:15:32 -0700

    Base cases passed but too slow to pass all cases

--- Leetcode/minimum_common_element/minimum_common_element.rb
+++ Leetcode/minimum_common_element/minimum_common_element.rb
@@ -1,6 +1,17 @@
 def get_common(nums1, nums2)
-  require 'pry'; binding.pry
+  common = []
+  nums1.each do |num|
+    nums2.each do |n|
+      if n == num
+        common << n
+      end
+    end
+  end
+  common.min
 end
 
 p get_common([1,2,3], [2,4])
-# p get_common([1,2,3,6], [2,3,4,5])
+p get_common([1,2,3,6], [2,3,4,5])
+
+
+# common << nums1.each { |num| nums2.each { |n| n == num ? n : 0}}
