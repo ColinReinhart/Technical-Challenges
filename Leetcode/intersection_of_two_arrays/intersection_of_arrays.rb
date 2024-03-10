812ba811ed2e6df50f739e16a42dd5fb1890515d
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-10 14:57:40 -0600

    Passing all cases

--- Leetcode/intersection_of_two_arrays/intersection_of_arrays.rb
+++ Leetcode/intersection_of_two_arrays/intersection_of_arrays.rb
@@ -1,6 +1,12 @@
+require 'set'
+
 def intersection(nums1, nums2)
-  require 'pry'; binding.pry
+  matched = []
+  set1 = nums1.to_set
+  set2 = nums2.to_set
+  set1.each { |num| matched << num if set2.include?(num) }
+  matched
 end
 
 p intersection([1,2,2,1], [2,2])
-# p intersection([4,9,5], [9,4,9,8,4])
+p intersection([4,9,5], [9,4,9,8,4])
