bd493fe53be305bb39210aa8339dd018cff68d1e
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-12 13:26:47 -0600

    Passing all test cases

--- Leetcode/remove_zero_sum_consecutive_nodes_from_linked_list/remove_zero_sum.rb
+++ Leetcode/remove_zero_sum_consecutive_nodes_from_linked_list/remove_zero_sum.rb
@@ -10,9 +10,57 @@ end
 # @return {ListNode}
 
 def remove_zero_sum_sublists(head)
-    require 'pry'; binding.pry
+  dummy = ListNode.new(0)
+  dummy.next = head
+  sum = 0
+  h = {}
+  cur = dummy
+  while cur
+    sum += cur.val
+    h[sum] = cur
+    cur = cur.next
+  end
+  sum = 0
+  cur = dummy
+  while cur
+    sum += cur.val
+    cur.next = h[sum].next
+    cur = cur.next
+  end
+  dummy.next
 end
 
-p remove_zero_sum_sublists([1,2,-3,3,1])
-p remove_zero_sum_sublists([1,2,3,-3,4])
-p remove_zero_sum_sublists([1,2,3,-3,-2])
+def create_linked_list(values)
+  return nil if values.empty?
+
+  head = ListNode.new(values[0])
+  current = head
+
+  (1...values.length).each do |i|
+    current.next = ListNode.new(values[i])
+    current = current.next
+  end
+
+  head
+end
+
+def print_linked_list(head)
+  current = head
+  while current
+    print current.val
+    print ' -> ' if current.next
+    current = current.next
+  end
+  puts
+end
+
+head1 = create_linked_list([1,2,-3,3,1])
+head2 = create_linked_list([1,2,3,-3,4])
+head3 = create_linked_list([1,2,3,-3,-2])
+
+puts "Example 1:"
+print_linked_list(head1)
+puts "Example 2:"
+print_linked_list(head2)
+puts "Example 3:"
+print_linked_list(head3)
