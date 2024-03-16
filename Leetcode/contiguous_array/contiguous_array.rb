e1028df8fff77a7da5b4fd469efc2cf2e3caaba8
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-16 13:33:07 -0600

    Passing base test cases, failing with [0,1,1,0,1,1,1,0]

--- Leetcode/contiguous_array/contiguous_array.rb
+++ Leetcode/contiguous_array/contiguous_array.rb
@@ -1,8 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_max_length(nums)
-  require 'pry'; binding.pry
+  nums.include?(0) && nums.include?(1) ? find_max_length_helper(nums) : 0
 end
 
-p find_max_length([0, 1]) # 2
+def find_max_length_helper(nums)
+  hash = Hash.new(0)
+  nums.each { |num| hash[num] += 1 }
+  hash.values.min * 2
+end
+
+# p find_max_length([0, 1]) # 2
 # p find_max_length([0, 1, 0]) # 2
+p find_max_length([0,1,1,0,1,1,1,0]) # 4
