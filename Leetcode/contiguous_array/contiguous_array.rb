324ca4a6a809f8416c410bf6a2eccc0413066823
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-16 13:34:57 -0600

    Passing all test cases

--- Leetcode/contiguous_array/contiguous_array.rb
+++ Leetcode/contiguous_array/contiguous_array.rb
@@ -5,11 +5,20 @@ def find_max_length(nums)
 end
 
 def find_max_length_helper(nums)
-  hash = Hash.new(0)
-  nums.each { |num| hash[num] += 1 }
-  hash.values.min * 2
+  max_length = 0
+  count = 0
+  hash = {0 => -1}
+  nums.each_with_index do |num, index|
+    count += num == 0 ? -1 : 1
+    if hash.key?(count)
+      max_length = [max_length, index - hash[count]].max
+    else
+      hash[count] = index
+    end
+  end
+  max_length
 end
 
-# p find_max_length([0, 1]) # 2
-# p find_max_length([0, 1, 0]) # 2
+p find_max_length([0, 1]) # 2
+p find_max_length([0, 1, 0]) # 2
 p find_max_length([0,1,1,0,1,1,1,0]) # 4
