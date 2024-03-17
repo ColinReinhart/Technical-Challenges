87b06ba496165c1c1bd715eac3f96a7aacb918df
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-17 15:02:00 -0600

    Stopping point because nap time is over

--- Leetcode/insert_interval/insert_interval.rb
+++ Leetcode/insert_interval/insert_interval.rb
@@ -2,36 +2,39 @@
 # @param {Integer[]} new_interval
 # @return {Integer[][]}
 def insert(intervals, new_interval)
-  full = []
-  answer = []
+  @full = []
+  @answer = []
   intervals.each do |int|
     arr = Array(int[0]..int[1])
-    full << arr
+    @full << arr
   end
-  full << full_new = Array(new_interval[0]..new_interval[1])
-  full = full.sort
-  full.each_with_index do |arr, i|
-    if full[i+1] == nil
-       full.each do |arr|
-        answer << [arr[0], arr[-1]]
+  @full << full_new = Array(new_interval[0]..new_interval[1])
+  @full = @full.sort
+  def combine_overlap(arr)
+    arr[1] == nil ? return @answer : 0 #This is where I left off 3:00 PM 3/17
+    if !(arr[0] & arr[1]).empty?
+      wip = arr[0] + arr[1]
+      if !(wip & arr[2]).empty?
+        @full.delete_at(0)
+        @full.delete_at(0)
+        @full.insert(0, wip.uniq)
+        combine_overlap(@full)
+      else
+        @answer << arr[0]
+        @full.shift
+        require 'pry'; binding.pry
       end
     else
-    arr.each do |num|
-        if full[i+1].include?(num)
-          new = full[i] + full[i+1]
-          full << new.uniq
-          full.delete_at(i+1)
-          full.delete_at(i)
-          full = full.sort
-        else
-        end
-      end
+      @answer << arr[0]
+      @full.shift
     end
   end
-  answer
+  until @full == []
+    combine_overlap(@full)
+  end
 end
 
-p insert([[1,3],[6,9]], [2,5]) # [[1,5],[6,9]]
+# p insert([[1,3],[6,9]], [2,5]) # [[1,5],[6,9]]
 p insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]) # [[1,2],[3,10],[12,16]]
 # p insert([], [5,7]) # [[5,7]]
 # p insert([[1,5]], [2,3]) # [[1,5]]
