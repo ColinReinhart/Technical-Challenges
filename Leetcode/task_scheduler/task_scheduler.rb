98cff14dbe020e3c86b35ff835d30909d4a310a1
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-22 14:27:42 -0600

    I was way off from the get go, but saving in case I want to reference this

--- Leetcode/task_scheduler/task_scheduler.rb
+++ Leetcode/task_scheduler/task_scheduler.rb
@@ -2,9 +2,23 @@
 # @param {Integer} n
 # @return {Integer}
 def least_interval(tasks, n)
-  require 'pry'; binding.pry
+  hash = { 'int' => n }
+  arr = []
+  tasks.each do |task|
+    hash[task] = hash[task].nil? ? 1 : hash[task] + 1
+  end
+  until hash.empty? do
+    hash.sort.map do |key, value|
+      arr << key
+      hash[key] -= 1
+      if hash[key] == 0
+        hash.delete(key)
+      end
+    end
+  end
+  arr.count
 end
 
 p least_interval(["A","A","A","B","B","B"], 2)
-# p least_interval(["A","A","A","B","B","B"], 0)
+p least_interval(["A","A","A","B","B","B"], 0)
 # p least_interval(["A","A","A","A","A","A","B","C","D","E","F","G"], 2)
