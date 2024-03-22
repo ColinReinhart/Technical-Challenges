f6cb17475542803ac77a4e60e08cad6b3e96a30c
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-22 11:40:59 -0600

    Passing all test cases

--- Leetcode/reverse_linked_list/reverse_linked_list.rb
+++ Leetcode/reverse_linked_list/reverse_linked_list.rb
@@ -9,11 +9,16 @@ end
 # @param {ListNode} head
 # @return {ListNode}
 def reverse_list(head)
-  require 'pry'; binding.pry
+  arr = []
+  while head
+    arr << head.val
+    head = head.next
+  end
+  arr.reverse!
 end
 
 head1 = ListNode.new(1, ListNode.new(2, ListNode.new(3, ListNode.new(4, ListNode.new(5)))))
 head2 = ListNode.new(1, ListNode.new(2))
 
 p reverse_list(head1)
-# p reverse_list(head2)
+p reverse_list(head2)
