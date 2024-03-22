7a73020c36f8c7f1c8228aaca00b10925cc4aa87
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-22 14:14:12 -0600

    Setup Task Scheduler

---
+++ Leetcode/task_scheduler/task_scheduler.rb
@@ -0,0 +1,10 @@
+# @param {Character[]} tasks
+# @param {Integer} n
+# @return {Integer}
+def least_interval(tasks, n)
+  require 'pry'; binding.pry
+end
+
+p least_interval(["A","A","A","B","B","B"], 2)
+# p least_interval(["A","A","A","B","B","B"], 0)
+# p least_interval(["A","A","A","A","A","A","B","C","D","E","F","G"], 2)
