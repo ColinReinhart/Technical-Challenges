344f3ed2cdbff96526c990aa094a4d4ab1c8b06d
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-23 13:56:29 -0600

    Add Reorder List to README

--- Leetcode/task_scheduler/task_scheduler.rb
+++ Leetcode/task_scheduler/task_scheduler.rb
@@ -9,7 +9,7 @@ def least_interval(tasks, n)
   until hash.empty? do
     hash.sort.map do |k,v|
       hash[k] -= 1
-      hash.delete(k) if hash[k] == 0
+      hash.delete(k) && wait += 1 if hash[k] == 0
       count += 1
     end
     count += wait if hash.size > 0 && wait > 0
@@ -17,6 +17,7 @@ def least_interval(tasks, n)
   count
 end
 
-p least_interval(["A","A","A","B","B","B"], 2)
-p least_interval(["A","A","A","B","B","B"], 0)
-p least_interval(["A","A","A", "B","B","B"], 3)
+# p least_interval(["A","A","A","B","B","B"], 2)
+# p least_interval(["A","A","A","B","B","B"], 0)
+# p least_interval(["A","A","A", "B","B","B"], 3)
+p least_interval(["A","A","A","B","B","B","C","D","E","F","G","H","I","J","K"], 7)
