a1180aa3d44d73514e9b80141800b3fb77d4a59b
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-24 12:55:21 -0600

    0(n) solution

--- Leetcode/squares_of_a_sorted_array/squares_of_a_sorted_array.rb
+++ Leetcode/squares_of_a_sorted_array/squares_of_a_sorted_array.rb
@@ -1,7 +1,23 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sorted_squares(nums)
-  nums.map { |num| num ** 2 }.sort
+  result = Array.new(nums.length)
+  start = 0
+  last = nums.length - 1
+  insert_position = last
+
+  while start <= last
+    if nums[start].abs > nums[last].abs
+      result[insert_position] = nums[start] ** 2
+      start += 1
+    else
+      result[insert_position] = nums[last] ** 2
+      last -= 1
+    end
+    insert_position -= 1
+  end
+
+  result
 end
 
 p sorted_squares([-4,-1,0,3,10]) == [0,1,9,16,100]
