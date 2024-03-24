8c6d2957d2ea2ace98cf5c6f684d79f919675c6f
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-24 12:47:54 -0600

    Passing all test cases

--- Leetcode/squares_of_a_sorted_array/squares_of_a_sorted_array.rb
+++ Leetcode/squares_of_a_sorted_array/squares_of_a_sorted_array.rb
@@ -1,8 +1,8 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sorted_squares(nums)
-  require 'pry'; binding.pry
+  nums.map { |num| num ** 2 }.sort
 end
 
-p sorted_squares([-4,-1,0,3,10])
-p sorted_squares([-7,-3,2,3,11])
+p sorted_squares([-4,-1,0,3,10]) == [0,1,9,16,100]
+p sorted_squares([-7,-3,2,3,11]) == [4,9,9,49,121]
