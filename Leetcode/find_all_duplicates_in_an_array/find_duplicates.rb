2c6d27005911908b191c793056083bc87721c9ff
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-25 12:56:40 -0600

    Passing all test cases

--- Leetcode/find_all_duplicates_in_an_array/find_duplicates.rb
+++ Leetcode/find_all_duplicates_in_an_array/find_duplicates.rb
@@ -1,7 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_duplicates(nums)
-    require 'pry'; binding.pry
+    hash = Hash.new(0)
+    nums.each { |num| hash[num] += 1 }
+    hash.select { |k,v| v > 1 }.keys
 end
 
 p find_duplicates([4,3,2,7,8,2,3,1]) # [2,3]
