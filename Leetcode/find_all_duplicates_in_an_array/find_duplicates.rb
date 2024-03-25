becdd97d9894f424ca13ec8f1ba1b68c5219d7d9
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-25 12:54:37 -0600

    Setup Find All Duplucates in an Array

---
+++ Leetcode/find_all_duplicates_in_an_array/find_duplicates.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer[]}
+def find_duplicates(nums)
+    require 'pry'; binding.pry
+end
+
+p find_duplicates([4,3,2,7,8,2,3,1]) # [2,3]
+p find_duplicates([1,1,2]) # [1]
+p find_duplicates([1]) # []
