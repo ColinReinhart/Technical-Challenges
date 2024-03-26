8f344d7f80a89443cf361a85567be5acff74379f
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-26 12:58:56 -0600

    Passing base cases

--- Leetcode/first_missing_positive/first_missing_positive.rb
+++ Leetcode/first_missing_positive/first_missing_positive.rb
@@ -1,7 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def first_missing_positive(nums)
-  require 'pry'; binding.pry
+  @x = 1
+  sorted = nums.sort
+  sort_pos = sorted.select { |num| num > 0 }
+  def find_missing(arr)
+    if arr[0] != @x
+      return @x
+    else arr[0] == @x
+      @x += 1
+      arr.shift
+      find_missing(arr)
+    end
+  end
+  find_missing(sort_pos)
 end
 
 p first_missing_positive([1,2,0]) # 3
