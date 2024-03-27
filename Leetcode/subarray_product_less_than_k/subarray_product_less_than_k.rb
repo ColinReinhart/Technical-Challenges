137b44b5390294d313a61b886d6e769a7597f564
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-27 09:58:02 -0600

    Setup Subarray Product Less Than K

---
+++ Leetcode/subarray_product_less_than_k/subarray_product_less_than_k.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def num_subarray_product_less_than_k(nums, k)
+  require 'pry'; binding.pry
+end
+
+p num_subarray_product_less_than_k([10, 5, 2, 6], 100)
+p num_subarray_product_less_than_k([1, 2, 3], 0)
