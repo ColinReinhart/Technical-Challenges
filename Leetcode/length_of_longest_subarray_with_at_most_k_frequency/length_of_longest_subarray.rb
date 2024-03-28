7dbda49a5cee3ee9bf83e97dfe7b2eb3e11f9b36
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-28 09:44:10 -0600

    Passing base cases, but I think it will fail full test because it does not take into account when a number appears less than k times

--- Leetcode/length_of_longest_subarray_with_at_most_k_frequency/length_of_longest_subarray.rb
+++ Leetcode/length_of_longest_subarray_with_at_most_k_frequency/length_of_longest_subarray.rb
@@ -2,9 +2,20 @@
 # @param {Integer} k
 # @return {Integer}
 def max_subarray_length(nums, k)
-  require 'pry'; binding.pry
+  subarray = []
+  hash = Hash.new(0)
+  nums.each { |num| hash[num] += 1 }
+  k.times do
+    hash.each do |key, value|
+      if value > 0
+        subarray << key
+        hash[key] -= 1
+      end
+    end
+  end
+  subarray.count
 end
 
 puts max_subarray_length([1,2,3,1,2,3,1,2], 2)
-# puts max_subarray_length([1,2,1,2,1,2,1,2], 1)
-# puts max_subarray_length([5,5,5,5,5,5,5], 4)
+puts max_subarray_length([1,2,1,2,1,2,1,2], 1)
+puts max_subarray_length([5,5,5,5,5,5,5], 4)
