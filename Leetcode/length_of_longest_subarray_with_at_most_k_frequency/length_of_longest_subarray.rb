623d5c8de1cb4e793b9e842b0bac06e7788c25dd
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-28 09:21:11 -0600

    Setup Lnegth of Longest Subarray...

---
+++ Leetcode/length_of_longest_subarray_with_at_most_k_frequency/length_of_longest_subarray.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def max_subarray_length(nums, k)
+  require 'pry'; binding.pry
+end
+
+puts max_subarray_length([1,2,3,1,2,3,1,2], 2)
+# puts max_subarray_length([1,2,1,2,1,2,1,2], 1)
+# puts max_subarray_length([5,5,5,5,5,5,5], 4)
