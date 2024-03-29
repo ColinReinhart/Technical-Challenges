33204d00cb8b8e7a1647c6511feb1b3538f03d0a
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-29 09:52:35 -0600

    Setup

---
+++ Leetcode/count_subarrays_where_max_element_appears_at_least_k_times/count_subarrays_max_element_k_times.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def count_subarrays(nums, k)
+  require 'pry'; binding.pry
+end
+
+p count_subarrays([1,3,2,3,3], 2) # 6
+# p count_subarrays([1,4,2,1], 3) # 0
