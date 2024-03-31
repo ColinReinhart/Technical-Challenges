7cfdc72917346030fd65e23cbe96aef85afe717a
Colin Reinhart <colinbreinhart@gmail.com>
2024-03-31 14:16:57 -0600

    Passing base cases, timing out on large dataset

--- Leetcode/count_subarrays_where_max_element_appears_at_least_k_times/count_subarrays_max_element_k_times.rb
+++ Leetcode/count_subarrays_where_max_element_appears_at_least_k_times/count_subarrays_max_element_k_times.rb
@@ -2,8 +2,33 @@
 # @param {Integer} k
 # @return {Integer}
 def count_subarrays(nums, k)
-  require 'pry'; binding.pry
+  return 0 if k > nums.length
+  arr = []
+  hash = Hash.new(0)
+  count = 0
+  max = nums.max
+  wip = k-1
+  left = 0
+  right = wip
+
+  until left == 0 && right == nums.length do
+    while right < nums.length do
+      # require 'pry'; binding.pry
+      nums[left..right].each { |num| hash[num] += 1 }
+      if hash[max] >= k
+        count += 1
+        arr << nums[left..right]
+      end
+      left += 1
+      right += 1
+      hash = Hash.new(0)
+    end
+    left = 0
+    wip += 1
+    right = wip
+  end
+  return count
 end
 
 p count_subarrays([1,3,2,3,3], 2) # 6
-# p count_subarrays([1,4,2,1], 3) # 0
+p count_subarrays([1,4,2,1], 3) # 0
