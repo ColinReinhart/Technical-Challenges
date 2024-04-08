cc97ab28e8f0e18a9e27dcca31571500ef8bea7a
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-08 09:42:46 -0600

    Passing all test cases

--- Leetcode/number_of_students_unable_to_eat_lunch/number_of_students.rb
+++ Leetcode/number_of_students_unable_to_eat_lunch/number_of_students.rb
@@ -2,8 +2,16 @@
 # @param {Integer[]} sandwiches
 # @return {Integer}
 def count_students(students, sandwiches)
-    require 'pry'; binding.pry
+  while students.include?(sandwiches[0])
+    if students[0] == sandwiches[0]
+      sandwiches.shift
+      students.shift
+    else
+      students << students.shift
+    end
+  end
+  students.length
 end
 
 p count_students([1,1,0,0], [0,1,0,1]) # 0
-# p count_students([1,1,1,0,0,1], [1,0,0,0,1,1]) # 3
+p count_students([1,1,1,0,0,1], [1,0,0,0,1,1]) # 3
