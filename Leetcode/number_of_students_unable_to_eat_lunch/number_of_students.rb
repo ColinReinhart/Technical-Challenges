0983be51d7de0d275038f9f2daeffac12210e3b3
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-08 09:23:15 -0600

    Setup Sandwiches

---
+++ Leetcode/number_of_students_unable_to_eat_lunch/number_of_students.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} students
+# @param {Integer[]} sandwiches
+# @return {Integer}
+def count_students(students, sandwiches)
+    require 'pry'; binding.pry
+end
+
+p count_students([1,1,0,0], [0,1,0,1]) # 0
+# p count_students([1,1,1,0,0,1], [1,0,0,0,1,1]) # 3
