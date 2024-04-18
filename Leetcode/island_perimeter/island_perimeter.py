03e026c00a0513b155ceb00ddbb32ef3fb9128e1
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-18 15:03:01 -0600

    Setup python solution

---
+++ Leetcode/island_perimeter/island_perimeter.py
@@ -0,0 +1,12 @@
+import pdb
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        count = 0
+        
+        for x in grid:
+            pdb.set_trace()
+        
+
+solution = Solution()
+solution.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])
