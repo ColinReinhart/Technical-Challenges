ed31874689b478b640d3d960638eb5f0ce4aec60
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-18 15:36:38 -0600

    Passing all test cases in PYYYTTTHHHOOONNNNNNNNNNNN!!!

--- Leetcode/island_perimeter/island_perimeter.py
+++ Leetcode/island_perimeter/island_perimeter.py
@@ -4,9 +4,24 @@ class Solution(object):
     def islandPerimeter(self, grid):
         count = 0
         
-        for x in grid:
-            pdb.set_trace()
+        for index, arr in enumerate(grid):
+            for indx, num in enumerate(arr):
+                if num == 1:
+                    count += 4
+                    if indx + 1 < len(arr) and arr[indx + 1] == 1:
+                        count -= 1
+                    if indx - 1 > -1 and arr[indx - 1] == 1:
+                        count -= 1
+                    if index > 0 and grid[index - 1][indx] == 1:
+                        count -= 1
+                    if index + 1 < len(grid) and grid[index + 1][indx] == 1:
+                        count -= 1
+                
+        return count
         
 
 solution = Solution()
-solution.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])
+print(solution.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+print(solution.islandPerimeter([[1]]))
+print(solution.islandPerimeter([[1,0]]))
+print(solution.islandPerimeter([[1],[1]]))
