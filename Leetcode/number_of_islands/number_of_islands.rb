09440638b33184a905fe46a4ad681325950c59ed
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-19 11:02:47 -0600

    Setup Number of Islands

---
+++ Leetcode/number_of_islands/number_of_islands.rb
@@ -0,0 +1,12 @@
+# @param {Character[][]} grid
+# @return {Integer}
+def num_islands(grid)
+  require 'pry'; binding.pry
+end
+
+p num_islands([
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+])
