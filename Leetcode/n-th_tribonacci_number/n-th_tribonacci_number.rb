53f88b5fafcd88b3da7cb3f726aba3c8332c78a0
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-24 09:30:01 -0600

    Beats all cases but feels a bit hard coded

--- Leetcode/n-th_tribonacci_number/n-th_tribonacci_number.rb
+++ Leetcode/n-th_tribonacci_number/n-th_tribonacci_number.rb
@@ -1,8 +1,18 @@
 # @param {Integer} n
 # @return {Integer}
 def tribonacci(n)
-  require 'pry'; binding.pry
+  if n == 0 || n == 1
+    return n
+  elsif n == 2
+    return 1
+  end
+  arr = [0, 1, 1]
+  x = n - 3
+  x.times do
+    arr << arr[-3..-1].sum
+  end
+  return arr[-3..-1].sum
 end
 
-p tribonacci(4)
-# p tribonacci(25)
+# p tribonacci(4)
+p tribonacci(25)
