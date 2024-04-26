47e0b5e3c0e1d7fe0e6c8660e11e50de47f773ad
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-26 15:03:05 -0600

    Passing all test cases

--- Leetcode/minimum_falling_path_sum_II/minimum_falling_path.rb
+++ Leetcode/minimum_falling_path_sum_II/minimum_falling_path.rb
@@ -1,7 +1,24 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def min_falling_path_sum(grid)
-  require 'pry'; binding.pry
+  n = grid.size
+  m = grid[0].size
+
+  dp = Array.new(n) { Array.new(m, 0) }
+
+  grid[0].each_with_index { |val, j| dp[0][j] = val }
+
+  (1...n).each do |i|
+    (0...m).each do |j|
+      min_sum = Float::INFINITY
+      (0...m).each do |k|
+        min_sum = [min_sum, dp[i - 1][k]].min if k != j
+      end
+      dp[i][j] = grid[i][j] + min_sum
+    end
+  end
+
+  dp[-1].min
 end
 
 p min_falling_path_sum([[1,2,3],[4,5,6],[7,8,9]])
