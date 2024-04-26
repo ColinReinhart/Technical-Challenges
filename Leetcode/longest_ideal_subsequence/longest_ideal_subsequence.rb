5bd3d82660f496580505e657b6af6fb41f0fc984
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-26 10:20:00 -0600

    Passing base test cases

--- Leetcode/longest_ideal_subsequence/longest_ideal_subsequence.rb
+++ Leetcode/longest_ideal_subsequence/longest_ideal_subsequence.rb
@@ -2,7 +2,23 @@
 # @param {Integer} k
 # @return {Integer}
 def longest_ideal_string(s, k)
-  require 'pry'; binding.pry
+  arr = {}
+  wip = {}
+  hash = ('a'..'z').each_with_object({}).with_index(1) { |(char, h), pos| h[char] = pos }
+  split = s.split('')
+  split.each { |char| wip[char] = hash[char] }
+
+  wip.each_with_index do | (key, v), index |
+    if arr == {}
+      arr[key] = v
+    elsif v - arr.values[- 1] <= k
+      arr[key] = v
+    end
+  end
+
+    arr.length
+
 end
 
 p longest_ideal_string("acfgbd", 2)
+p longest_ideal_string("abcd", 3)
