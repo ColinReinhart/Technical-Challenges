63056ba51dcb6c29b021d3a317a568bbfc246727
Colin Reinhart <colinbreinhart@gmail.com>
2024-04-26 14:59:27 -0600

    Setup Minimum Falling Path Sum II

---
+++ Leetcode/minimum_falling_path_sum_II/minimum_falling_path.rb
@@ -0,0 +1,8 @@
+# @param {Integer[][]} grid
+# @return {Integer}
+def min_falling_path_sum(grid)
+  require 'pry'; binding.pry
+end
+
+p min_falling_path_sum([[1,2,3],[4,5,6],[7,8,9]])
+p min_falling_path_sum([[7]])
