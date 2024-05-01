be8a352d07279239758600201bb2725f59531eb5
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-01 14:21:39 -0600

    Passing all test cases

--- Leetcode/reverse_prefix_of_word/reverse_prefix_of_word.rb
+++ Leetcode/reverse_prefix_of_word/reverse_prefix_of_word.rb
@@ -2,9 +2,21 @@
 # @param {Character} ch
 # @return {String}
 def reverse_prefix(word, ch)
-  require 'pry'; binding.pry
+  if word.include?(ch)
+    split = word.split('')
+    answer = []
+    split.each_with_index do |c, i|
+      if c == ch
+        answer << split[0..i].reverse
+        answer << split[i+1..-1]
+        return answer.join
+      end
+    end
+  else
+    return word
+  end
 end
 
 p reverse_prefix("abcdefd", "d")
-# p reverse_prefix("xyxzxe", "z")
-# p reverse_prefix("abcd", "z")
+p reverse_prefix("xyxzxe", "z")
+p reverse_prefix("abcd", "z")
