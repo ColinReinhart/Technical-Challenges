f140db757638d0dc9ccc5631bbc947793835273a
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-01 11:57:56 -0600

    Setup Reverse Prefix of Word

---
+++ Leetcode/reverse_prefix_of_word/reverse_prefix_of_word.rb
@@ -0,0 +1,10 @@
+# @param {String} word
+# @param {Character} ch
+# @return {String}
+def reverse_prefix(word, ch)
+  require 'pry'; binding.pry
+end
+
+p reverse_prefix("abcdefd", "d")
+# p reverse_prefix("xyxzxe", "z")
+# p reverse_prefix("abcd", "z")
