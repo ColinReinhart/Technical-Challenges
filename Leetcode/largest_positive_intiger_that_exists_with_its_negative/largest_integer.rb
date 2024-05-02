6ccedbb3cb6b7559805aa84e61f15c721ec89b66
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-02 11:03:53 -0600

    Passing all test cases

--- Leetcode/largest_positive_intiger_that_exists_with_its_negative/largest_integer.rb
+++ Leetcode/largest_positive_intiger_that_exists_with_its_negative/largest_integer.rb
@@ -1,9 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_max_k(nums)
-  require 'pry'; binding.pry
+  has_negative = []
+  nums.each { |num| has_negative << num if nums.include?(num * -1) }
+  has_negative.max || -1
 end
 
 p find_max_k([-1,2,-3,3])
-# p find_max_k([-1,10,6,7,-7,1])
-# p find_max_k([-10,8,6,7,-2,-3])
+p find_max_k([-1,10,6,7,-7,1])
+p find_max_k([-10,8,6,7,-2,-3])
