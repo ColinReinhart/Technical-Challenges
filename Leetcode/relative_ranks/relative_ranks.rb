a85f09ed7be9fbb60cd495782eb9b75ad2dd5a9c
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-08 09:39:48 -0600

    Passing all test cases

--- Leetcode/relative_ranks/relative_ranks.rb
+++ Leetcode/relative_ranks/relative_ranks.rb
@@ -1,8 +1,22 @@
 # @param {Integer[]} score
 # @return {String[]}
 def find_relative_ranks(score)
-    require 'pry'; binding.pry
+  hash = {}
+  answer = []
+  score.sort.reverse.each_with_index { |score, index| hash[score] = index + 1 }
+  score.each do |player|
+    if hash[player] == 1
+      answer << "Gold Medal"
+    elsif hash[player] == 2
+      answer << "Silver Medal"
+    elsif hash[player] == 3
+      answer << "Bronze Medal"
+    else
+      answer << hash[player].to_s
+    end
+  end
+  answer
 end
 
-# p find_relative_ranks([5,4,3,2,1])
+p find_relative_ranks([5,4,3,2,1])
 p find_relative_ranks([10,3,8,9,4])
