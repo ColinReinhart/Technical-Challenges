fc55d998819a228dd035f9d8c7b125c99e01719f
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-08 09:25:48 -0600

    Setup Relative Ranks;

---
+++ Leetcode/relative_ranks/relative_ranks.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} score
+# @return {String[]}
+def find_relative_ranks(score)
+    require 'pry'; binding.pry
+end
+
+# p find_relative_ranks([5,4,3,2,1])
+p find_relative_ranks([10,3,8,9,4])
