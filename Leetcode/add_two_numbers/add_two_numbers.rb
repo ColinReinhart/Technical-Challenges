fc75ff5cbe9ffd88f9a73b77308f2e5b361fd11e
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-08 15:42:31 -0600

    Passing base cases

--- Leetcode/add_two_numbers/add_two_numbers.rb
+++ Leetcode/add_two_numbers/add_two_numbers.rb
@@ -1,19 +1,33 @@
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
 # @param {ListNode} l1
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
-  [l1.reverse.join.to_i + l2.reverse.join.to_i].to_s.split('').reverse.map(&:to_i)[1..-2]
+  @array = []
+
+  def arr_ll(ll)
+    arr = []
+    wip = ll
+    until wip == nil do
+      arr << wip.val
+      wip = wip.next
+    end
+    @array << arr.join('').to_i
+  end
+  arr_ll(l1)
+  arr_ll(l2)
+  @array.sum.to_s.split('').reverse.map { |s| s.to_i }
 end
 
-
-p add_two_numbers([2,4,3], [5,6,4])
-p add_two_numbers([0], [0])
-p add_two_numbers([9,9,9,9,9,9,9], [9,9,9,9])
+list1 = ListNode.new(2, ListNode.new(4, ListNode.new(3, ListNode.new)))
+list2 = ListNode.new(5, ListNode.new(6, ListNode.new(4, ListNode.new)))
+p add_two_numbers(list1, list2)
+# p add_two_numbers([0], [0])
+# p add_two_numbers([9,9,9,9,9,9,9], [9,9,9,9])
