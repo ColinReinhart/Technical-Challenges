66483a36bdecb372d052ea1659878e3b4793d4e2
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-09 10:55:52 -0600

    Passing Base Cases

--- Leetcode/maximize_happiness_of_selected_children/maximize_happiness.rb
+++ Leetcode/maximize_happiness_of_selected_children/maximize_happiness.rb
@@ -2,10 +2,22 @@
 # @param {Integer} k
 # @return {Integer}
 def maximum_happiness_sum(happiness, k)
-  require 'pry'; binding.pry
+  arr = []
+  sort = happiness.sort
+  k.times do
+    temp = []
+    arr << sort.pop
+    sort.map do |n|
+      if n > 0
+       temp << n -= 1
+      end
+      sort = temp
+    end
+  end
+  arr.sum
 end
 
 
 p maximum_happiness_sum([1,2,3], 2)
-# p maximum_happiness_sum([1,1,1,1], 2)
-# p maximum_happiness_sum([2,3,4,5], 1)
+p maximum_happiness_sum([1,1,1,1], 2)
+p maximum_happiness_sum([2,3,4,5], 1)
