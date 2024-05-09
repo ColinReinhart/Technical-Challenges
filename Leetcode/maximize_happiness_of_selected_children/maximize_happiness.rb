6a3b8e836dd3e0fe71355d86436931ee0d68ce24
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-09 10:48:40 -0600

    Setup Maximize Happiness

---
+++ Leetcode/maximize_happiness_of_selected_children/maximize_happiness.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} happiness
+# @param {Integer} k
+# @return {Integer}
+def maximum_happiness_sum(happiness, k)
+  require 'pry'; binding.pry
+end
+
+
+p maximum_happiness_sum([1,2,3], 2)
+# p maximum_happiness_sum([1,1,1,1], 2)
+# p maximum_happiness_sum([2,3,4,5], 1)
