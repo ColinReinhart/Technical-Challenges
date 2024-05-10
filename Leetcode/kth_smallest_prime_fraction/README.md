b831421b0684628755c3d6f923ef81329e22bb84
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-10 15:40:46 -0600

    Passing base test cases

--- Leetcode/kth_smallest_prime_fraction/README.md
+++ Leetcode/kth_smallest_prime_fraction/README.md
@@ -17,6 +17,7 @@ Output: [2,5]
 Explanation: The fractions to be considered in sorted order are:
 1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
 The third fraction is 2/5.
+
 Example 2:
 
 Input: arr = [1,7], k = 1
--- Leetcode/kth_smallest_prime_fraction/kth_smallest_prime_fraction.rb
+++ Leetcode/kth_smallest_prime_fraction/kth_smallest_prime_fraction.rb
@@ -2,8 +2,27 @@
 # @param {Integer} k
 # @return {Integer[]}
 def kth_smallest_prime_fraction(arr, k)
-  require 'pry'; binding.pry
+  hash = {}
+  array = []
+  floats = []
+  wip = []
+  sorted_values = []
+  arr.each_with_index do |n, i|
+    arr[i+1..-1].each do |num|
+      array << [n, num]
+    end
+  end
+  array.each do |frac|
+    hash[frac] = (frac[0].to_f)/(frac[1].to_f)
+  end
+  hash.each do |k,v|
+    floats << v
+  end
+  floats.sort.each do |fl|
+    wip << hash.key(fl)
+  end
+  return wip[k-1]
 end
 
 p kth_smallest_prime_fraction([1,2,3,5], 3)
-# p kth_smallest_prime_fraction([1,7], 1)
+p kth_smallest_prime_fraction([1,7], 1)
