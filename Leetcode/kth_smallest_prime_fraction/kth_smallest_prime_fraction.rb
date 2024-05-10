12ded5d9f9e35e9b828ce35f22704391bce2b2c8
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-10 14:14:37 -0600

    Setup Kth Smallest Prime Fraction

---
+++ Leetcode/kth_smallest_prime_fraction/kth_smallest_prime_fraction.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} arr
+# @param {Integer} k
+# @return {Integer[]}
+def kth_smallest_prime_fraction(arr, k)
+  require 'pry'; binding.pry
+end
+
+p kth_smallest_prime_fraction([1,2,3,5], 3)
+# p kth_smallest_prime_fraction([1,7], 1)
