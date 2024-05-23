9725e21b2bf09a2d4bc3b6d074d73de59d40294e
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-23 15:11:53 -0600

    Passing all test cases

--- Leetcode/the_number_of_beautiful_subsets/number_of_beautiful_subsets.rb
+++ Leetcode/the_number_of_beautiful_subsets/number_of_beautiful_subsets.rb
@@ -2,28 +2,40 @@
 # @param {Integer} k
 # @return {Integer}
 def beautiful_subsets(nums, k)
-  output = []
-  arr = []
-  (0..nums.length - 1).each do |start_point|
-    (start_point..nums.length - 1).each do |end_point|
-      arr << nums[start_point..end_point]
-    end
-  end
-
-  arr.map do |a|
-    if a.length == 1
-      output << a
-    else
-      a.combination(2) do |x, y|
-        if y-x != k
-          output << [x, y]
-        end
-      end
-    end
-  end
-
-  return output.length
+    @ans = 0
+    @nums = nums
+    @k = k
+    backtracking
+    @ans
 end
 
-p beautiful_subsets([2,4,6], 2)
-p beautiful_subsets([1], 1)
+def backtracking index = 0, included = Hash.new(0)
+    # When reaching end of nums exit recursion.
+    if index >= @nums.size
+        return
+    end
+
+    while index < @nums.size
+        # When we have numbers in our hash and there exist not number where @nums[index] minus or plus
+        # would give us k, continue.
+        # For example if @nums[index] = 4 and @k = 2..
+        # 4 - 2 = 2(k), so we cannot have 2 in our hash or else it won't be beautiful.
+        # 4 + 2 = 6, if we have 6 in our hash then it can't be beautiful because 6-4 = 2(k).
+        if (included.size > 0) and ( (included[@nums[index] - @k] > 0) or (included[@nums[index] + @k] > 0) )
+
+        else
+            # do
+            included[@nums[index]] += 1
+            @ans += 1
+            backtracking(index + 1, included)
+            # undo
+            included[@nums[index]] -= 1
+        end
+
+        index += 1
+    end
+end
+
+# p beautiful_subsets([2,4,6], 2)
+# p beautiful_subsets([1], 1)
+p beautiful_subsets([4,2,5,9,10,3], 1)
