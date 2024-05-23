a988ae4776dd82f57118b65dd5333b0d34d341ff
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-23 10:03:01 -0600

    Setup The Number of Beautiful Subsets

---
+++ Leetcode/the_number_of_beautiful_subsets/number_of_beautiful_subsets.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer}
+def beautiful_subsets(nums, k)
+  require 'pry'; binding.pry
+end
+
+p beautiful_subsets([2,4,6], 2)
+# p beautiful_subsets([1], 1)
