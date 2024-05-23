78f0bc8c461d8901886ca312b7c475680cf59440
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-23 11:28:29 -0600

    Passing base test cases

--- Leetcode/the_number_of_beautiful_subsets/number_of_beautiful_subsets.rb
+++ Leetcode/the_number_of_beautiful_subsets/number_of_beautiful_subsets.rb
@@ -2,8 +2,28 @@
 # @param {Integer} k
 # @return {Integer}
 def beautiful_subsets(nums, k)
-  require 'pry'; binding.pry
+  output = []
+  arr = []
+  (0..nums.length - 1).each do |start_point|
+    (start_point..nums.length - 1).each do |end_point|
+      arr << nums[start_point..end_point]
+    end
+  end
+
+  arr.map do |a|
+    if a.length == 1
+      output << a
+    else
+      a.combination(2) do |x, y|
+        if y-x != k
+          output << [x, y]
+        end
+      end
+    end
+  end
+
+  return output.length
 end
 
 p beautiful_subsets([2,4,6], 2)
-# p beautiful_subsets([1], 1)
+p beautiful_subsets([1], 1)
