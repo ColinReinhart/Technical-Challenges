b500d673dcdf38b0dd8138ebf14316068eb5437d
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-29 14:45:47 -0600

    Setup Zigzag Conversion

---
+++ Leetcode/zigzag_conversion/zigzag_conversion.rb
@@ -0,0 +1,10 @@
+# @param {String} s
+# @param {Integer} num_rows
+# @return {String}
+def convert(s, num_rows)
+  require 'pry'; binding.pry
+end
+
+p convert("PAYPALISHIRING", 3)
+# p convert("PAYPALISHIRING", 4)
+# p convert("A", 1)
