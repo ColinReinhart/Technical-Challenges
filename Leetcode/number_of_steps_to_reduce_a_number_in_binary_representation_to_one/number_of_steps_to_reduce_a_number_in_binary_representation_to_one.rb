067a48c6bbee09a83fcd79ff3cb4147f91fef1d5
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-29 11:14:27 -0600

    Passing all test cases

--- Leetcode/number_of_steps_to_reduce_a_number_in_binary_representation_to_one/number_of_steps_to_reduce_a_number_in_binary_representation_to_one.rb
+++ Leetcode/number_of_steps_to_reduce_a_number_in_binary_representation_to_one/number_of_steps_to_reduce_a_number_in_binary_representation_to_one.rb
@@ -2,7 +2,20 @@
 # @return {Integer}
 def num_steps(s)
   n = s.to_i(2)
-  require 'pry'; binding.pry
+  count = 0
+
+  until n == 1 do
+    if n.odd?
+      n += 1
+      count += 1
+    else
+      n = n / 2
+      count += 1
+    end
+  end
+  count
 end
 
 p num_steps("1101")
+p num_steps("10")
+p num_steps("1")
