8efef9ed01684e953631312f4b186405173029d5
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-30 11:28:01 -0600

    Passing all test cases

--- Leetcode/reverse_integer/reverse_integer.rb
+++ Leetcode/reverse_integer/reverse_integer.rb
@@ -1,9 +1,16 @@
 # @param {Integer} x
 # @return {Integer}
 def reverse(x)
-  require 'pry'; binding.pry
+  rev = x.to_s.reverse.to_i
+  if x.negative? && rev > -2147483648 && rev < 2147483648
+    return -rev
+  elsif rev > -2147483648 && rev < 2147483648
+    return rev
+  else
+    return 0
+  end
 end
 
 p reverse(123)
-# p reverse(-123)
-# p reverse(120)
+p reverse(-123)
+p reverse(120)
