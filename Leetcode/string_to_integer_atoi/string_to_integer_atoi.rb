71111a93f4a5e8f2f6e891c15f5161c667f991fc
Colin Reinhart <colinbreinhart@gmail.com>
2024-05-31 13:46:38 -0600

    Passing base test cases

--- Leetcode/string_to_integer_atoi/string_to_integer_atoi.rb
+++ Leetcode/string_to_integer_atoi/string_to_integer_atoi.rb
@@ -1,11 +1,11 @@
 # @param {String} s
 # @return {Integer}
 def my_atoi(s)
-  require 'pry'; binding.pry
+  return s.to_i
 end
 
-# p my_atoi("42") == 42
+p my_atoi("42") == 42
 p my_atoi(" -042") == -42
-# p my_atoi("1337c0d3") == 1337
-# p my_atoi("0-1") == 0
-# p my_atoi("words and 987") == 0
+p my_atoi("1337c0d3") == 1337
+p my_atoi("0-1") == 0
+p my_atoi("words and 987") == 0
