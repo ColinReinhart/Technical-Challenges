c8424bb5ebad5378c7660abed26329d122cddfe2
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-06 10:36:14 -0600

    Setup Hand of Straights

---
+++ Leetcode/hand_of_straights/hand_of_straights.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} hand
+# @param {Integer} group_size
+# @return {Boolean}
+def is_n_straight_hand(hand, group_size)
+    require 'pry'; binding.pry
+end
+
+p is_n_straight_hand([1,2,3,6,2,3,4,7,8], 3)
+# p is_n_straight_hand([1,2,3,4,5], 4)
