26c8de12b29d4f1111fe8f274a4b215f4c7c58b8
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-07 09:35:46 -0600

    Passing base cases, not passing test case 113

--- Leetcode/replace_words/replace_words.rb
+++ Leetcode/replace_words/replace_words.rb
@@ -2,7 +2,17 @@
 # @param {String} sentence
 # @return {String}
 def replace_words(dictionary, sentence)
-  require 'pry'; binding.pry
+  arr = []
+  split = sentence.split(" ")
+  split.each_with_index do |word, i|
+    dictionary.each do |root|
+      if word.split('')[0..root.length - 1].join('') == root
+        split[i] = root
+      end
+    end
+  end
+  split.join(' ')
 end
 
 p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
+p replace_words(["a","b","c"], "aadsfasf absbs bbab cadsfafs")
