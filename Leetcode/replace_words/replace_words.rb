ab4bf1c5ee2a17cee87742a6448a8950ec202145
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-07 10:04:30 -0600

    Passing all testcases

--- Leetcode/replace_words/replace_words.rb
+++ Leetcode/replace_words/replace_words.rb
@@ -1,20 +1,23 @@
-# @param {String[]} dictionary
-# @param {String} sentence
-# @return {String}
+require 'set'
+
 def replace_words(dictionary, sentence)
-  split = sentence.split(" ")
-  split.each_with_index do |word, i|
-    dictionary.sort.each do |root|
-      if word.split('')[0..root.length - 1].join('') == root
-        split[i] = root
+  dictionary = dictionary.sort
+  set = dictionary.to_set
+  words = sentence.split(" ")
+
+  words.map! do |word|
+    dictionary.each do |root|
+      if word.start_with?(root)
+        word = root
         break
       end
     end
+    word
   end
-  split.join(' ')
+  words.join(' ')
 end
 
-# p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
-# p replace_words(["a","b","c"], "aadsfasf absbs bbab cadsfafs")
-# p replace_words(["a", "aa", "aaa", "aaaa"], "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa")
+p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
+p replace_words(["a","b","c"], "aadsfasf absbs bbab cadsfafs")
+p replace_words(["a", "aa", "aaa", "aaaa"], "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa")
 p replace_words(["catt","cat","bat","rat"], "the cattle was rattled by the battery")
