8268b9fb1dec8fcababbbb5ac43560ce201fccdc
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-07 09:18:40 -0600

    Setup Replace Words

---
+++ Leetcode/replace_words/replace_words.rb
@@ -0,0 +1,8 @@
+# @param {String[]} dictionary
+# @param {String} sentence
+# @return {String}
+def replace_words(dictionary, sentence)
+  require 'pry'; binding.pry
+end
+
+p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
