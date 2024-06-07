2ea09db1be2e783f4ae8a332186ecf182e342c32
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-07 10:00:04 -0600

    Passing 113, but now time limit exceeding on testcase 114

--- Leetcode/replace_words/replace_words.rb
+++ Leetcode/replace_words/replace_words.rb
@@ -2,17 +2,19 @@
 # @param {String} sentence
 # @return {String}
 def replace_words(dictionary, sentence)
-  arr = []
   split = sentence.split(" ")
   split.each_with_index do |word, i|
-    dictionary.each do |root|
+    dictionary.sort.each do |root|
       if word.split('')[0..root.length - 1].join('') == root
         split[i] = root
+        break
       end
     end
   end
   split.join(' ')
 end
 
-p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
-p replace_words(["a","b","c"], "aadsfasf absbs bbab cadsfafs")
+# p replace_words(["cat","bat","rat"], "the cattle was rattled by the battery")
+# p replace_words(["a","b","c"], "aadsfasf absbs bbab cadsfafs")
+# p replace_words(["a", "aa", "aaa", "aaaa"], "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa")
+p replace_words(["catt","cat","bat","rat"], "the cattle was rattled by the battery")
