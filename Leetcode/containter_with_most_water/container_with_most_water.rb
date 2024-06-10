ff230421153f5ea0f270d6534e0b6e824edb46fb
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-10 15:16:24 -0600

    Passing base test cases, failing at case 51 for Time Limit

--- Leetcode/containter_with_most_water/container_with_most_water.rb
+++ Leetcode/containter_with_most_water/container_with_most_water.rb
@@ -1,8 +1,18 @@
 # @param {Integer[]} height
 # @return {Integer}
 def max_area(height)
-  require 'pry'; binding.pry
+  highest = 0
+  height.each_with_index do |h, i|
+    x = i + 1
+    until x == height.length
+      if [h, height[x]].min * (x - i) > highest
+        highest = [h, height[x]].min * (x - i)
+      end
+      x += 1
+    end
+  end
+  highest
 end
 
 p max_area([1,8,6,2,5,4,8,3,7])
-# p max_area([1,1])
+p max_area([1,1])
