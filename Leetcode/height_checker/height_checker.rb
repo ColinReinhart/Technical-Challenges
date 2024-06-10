3305f9bccd63565a56a4701fc37b3ccda6a6de46
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-10 14:20:20 -0600

    Passing all testcsases

--- Leetcode/height_checker/height_checker.rb
+++ Leetcode/height_checker/height_checker.rb
@@ -1,14 +1,17 @@
 # @param {Integer[]} heights
 # @return {Integer}
 def height_checker(heights)
-  require 'pry'; binding.pry
+  count = 0
+  sorted = heights.sort
+  heights.each_with_index { |x, i| x != sorted[i] ? count += 1 : 0 }
+  count
 end
 
 p height_checker([1,1,4,2,1,3])
 # => 3
 
-# p height_checker([5,1,2,3,4])
+p height_checker([5,1,2,3,4])
 # # => 5
 
-# p height_checker([1,2,3,4,5])
+p height_checker([1,2,3,4,5])
 # # => 0
