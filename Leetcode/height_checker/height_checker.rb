62ebd47644e35af7eab220430201337b8139f118
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-10 14:13:53 -0600

    Setup Height Checker

---
+++ Leetcode/height_checker/height_checker.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} heights
+# @return {Integer}
+def height_checker(heights)
+  require 'pry'; binding.pry
+end
+
+p height_checker([1,1,4,2,1,3])
+# => 3
+
+# p height_checker([5,1,2,3,4])
+# # => 5
+
+# p height_checker([1,2,3,4,5])
+# # => 0
