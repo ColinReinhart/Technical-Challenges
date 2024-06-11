4159e4635e2853eff998170dad37f18bb2f70d9d
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-11 14:36:49 -0600

    Setup Relative Sort Array

---
+++ Leetcode/relative_sort_array/relative_sort_array.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} arr1
+# @param {Integer[]} arr2
+# @return {Integer[]}
+def relative_sort_array(arr1, arr2)
+  require 'pry'; binding.pry
+end
+
+
+p relative_sort_array([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6])
+# p relative_sort_array([28,6,22,8,44,17], [22,28,8,6])
