7503b1000bd232bf357bb780fd4284d8521ec73f
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-12 10:29:57 -0600

    Passing all test cases

--- Leetcode/sort_colors/sort_colors.rb
+++ Leetcode/sort_colors/sort_colors.rb
@@ -1,20 +1,22 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def sort_colors(nums)
-  array = []
-  nums.each_with_index do |x,idx|
-    if array == []
-      array << x
-    else
-      array.each_with_index do |y,i|
-        if y >= x
-          array.insert(i,x)
-          break
-        end
-      end
+  low, mid, high = 0, 0, nums.length - 1
+
+  while mid <= high
+    case nums[mid]
+    when 0
+      nums[low], nums[mid] = nums[mid], nums[low]
+      low += 1
+      mid += 1
+    when 1
+      mid += 1
+    when 2
+      nums[mid], nums[high] = nums[high], nums[mid]
+      high -= 1
     end
   end
-  array
+  nums
 end
 
 p sort_colors([2,0,2,1,1,0])
