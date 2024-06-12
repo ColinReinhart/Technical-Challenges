0308a78620c128651389982aa3671815095748ed
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-12 09:36:41 -0600

    Setup Sort Colors

---
+++ Leetcode/sort_colors/sort_colors.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+def sort_colors(nums)
+  require 'pry'; binding.pry
+end
+
+p sort_colors([2,0,2,1,1,0])
+# p sort_colors([2,0,1])
