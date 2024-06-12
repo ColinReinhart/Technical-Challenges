c89f4b05b08d164e02bcd1ccf6e8b3fd36c15e2a
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-12 10:09:55 -0600

    Solved, but not in place

--- Leetcode/sort_colors/sort_colors.rb
+++ Leetcode/sort_colors/sort_colors.rb
@@ -1,8 +1,21 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def sort_colors(nums)
-  require 'pry'; binding.pry
+  array = []
+  nums.each_with_index do |x,idx|
+    if array == []
+      array << x
+    else
+      array.each_with_index do |y,i|
+        if y >= x
+          array.insert(i,x)
+          break
+        end
+      end
+    end
+  end
+  array
 end
 
 p sort_colors([2,0,2,1,1,0])
-# p sort_colors([2,0,1])
+p sort_colors([2,0,1])
