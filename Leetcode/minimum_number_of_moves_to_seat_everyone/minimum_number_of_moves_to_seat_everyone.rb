6f9ffa544292186c18e89c8a108ef79df7ddbdc5
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-13 09:52:55 -0600

    Setup Minimum Number of Moves to Seat Everyone

---
+++ Leetcode/minimum_number_of_moves_to_seat_everyone/minimum_number_of_moves_to_seat_everyone.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} seats
+# @param {Integer[]} students
+# @return {Integer}
+def min_moves_to_seat(seats, students)
+  require 'pry'; binding.pry
+end
+
+p min_moves_to_seat([3,1,5], [2,7,4]) # 4
+# p min_moves_to_seat([4,1,5,9], [1,3,2,6]) # 7
+# p min_moves_to_seat([2,2,6,6], [1,3,2,6]) # 4
