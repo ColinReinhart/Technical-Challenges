223dff763d83f7251f3151975af3b1f50d459ebb
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-13 11:01:08 -0600

    Passing all testcases

--- Leetcode/minimum_number_of_moves_to_seat_everyone/minimum_number_of_moves_to_seat_everyone.rb
+++ Leetcode/minimum_number_of_moves_to_seat_everyone/minimum_number_of_moves_to_seat_everyone.rb
@@ -2,9 +2,15 @@
 # @param {Integer[]} students
 # @return {Integer}
 def min_moves_to_seat(seats, students)
-  require 'pry'; binding.pry
+  x = seats.sort
+  y = students.sort
+  moves = 0
+
+  moves += (x.shift - y.shift).abs until x.empty?
+
+  moves
 end
 
 p min_moves_to_seat([3,1,5], [2,7,4]) # 4
-# p min_moves_to_seat([4,1,5,9], [1,3,2,6]) # 7
-# p min_moves_to_seat([2,2,6,6], [1,3,2,6]) # 4
+p min_moves_to_seat([4,1,5,9], [1,3,2,6]) # 7
+p min_moves_to_seat([2,2,6,6], [1,3,2,6]) # 4
