2b59079e40425ad83f4cfa85e0644f1866dd5c39
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-12 18:05:52 -0600

    Passing all testcases and Markdown

--- Leetcode/containter_with_most_water/container_with_most_water.rb
+++ Leetcode/containter_with_most_water/container_with_most_water.rb
@@ -1,17 +1,23 @@
-# @param {Integer[]} height
-# @return {Integer}
 def max_area(height)
-  highest = 0
-  height.each_with_index do |h, i|
-    x = i + 1
-    until x == height.length
-      if [h, height[x]].min * (x - i) > highest
-        highest = [h, height[x]].min * (x - i)
-      end
-      x += 1
+  left = 0
+  right = height.length - 1
+  max_area = 0
+
+  while left < right
+    # Calculate the area
+    current_area = [height[left], height[right]].min * (right - left)
+    # Update the maximum area found
+    max_area = [max_area, current_area].max
+
+    # Move the pointer pointing to the shorter line inward
+    if height[left] < height[right]
+      left += 1
+    else
+      right -= 1
     end
   end
-  highest
+
+  max_area
 end
 
 p max_area([1,8,6,2,5,4,8,3,7])
