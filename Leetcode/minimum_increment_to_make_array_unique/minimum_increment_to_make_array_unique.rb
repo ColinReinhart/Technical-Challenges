e10492e0cf8a9f3111d64ba56b358e0c6849e5a6
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-14 11:09:30 -0600

    Passing all test cases

--- Leetcode/minimum_increment_to_make_array_unique/minimum_increment_to_make_array_unique.rb
+++ Leetcode/minimum_increment_to_make_array_unique/minimum_increment_to_make_array_unique.rb
@@ -1,8 +1,17 @@
-# @param {Integer[]} nums
-# @return {Integer}
 def min_increment_for_unique(nums)
-  require 'pry'; binding.pry
+  nums.sort!
+  moves = 0
+
+  (1...nums.length).each do |i|
+    if nums[i] <= nums[i - 1]
+      increment = nums[i - 1] - nums[i] + 1
+      nums[i] += increment
+      moves += increment
+    end
+  end
+
+  moves
 end
 
-p min_increment_for_unique([1,2,2])
-# p min_increment_for_unique([3,2,1,2,1,7])
+puts min_increment_for_unique([1,2,2]) # Output: 1
+puts min_increment_for_unique([3,2,1,2,1,7]) # Output: 6
