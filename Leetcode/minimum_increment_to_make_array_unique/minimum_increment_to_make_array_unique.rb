96fce1610db23c4a33bebe9c815909b1016763da
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-14 09:42:19 -0600

    Setup Minimum Increment to Make Array Unique

---
+++ Leetcode/minimum_increment_to_make_array_unique/minimum_increment_to_make_array_unique.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def min_increment_for_unique(nums)
+  require 'pry'; binding.pry
+end
+
+p min_increment_for_unique([1,2,2])
+# p min_increment_for_unique([3,2,1,2,1,7])
