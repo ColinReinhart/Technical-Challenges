2315ee5d99edd1be4855da0a97ce21e7c6c0d957
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-15 13:16:14 -0600

    Setup IPO

---
+++ Leetcode/ipo/ipo.rb
@@ -0,0 +1,11 @@
+# @param {Integer} k
+# @param {Integer} w
+# @param {Integer[]} profits
+# @param {Integer[]} capital
+# @return {Integer}
+def find_maximized_capital(k, w, profits, capital)
+  require 'pry'; binding.pry
+end
+
+p find_maximized_capital(2, 0, [1,2,3], [0,1,1]) # 4
+# p find_maximized_capital(3, 0, [1,2,3], [0,1,2]) # 6
