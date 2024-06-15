950218056cb065ab73a4110d1a76eab5d0f96b06
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-15 14:34:15 -0600

    Passing all testcases

--- Leetcode/ipo/ipo.rb
+++ Leetcode/ipo/ipo.rb
@@ -4,8 +4,28 @@
 # @param {Integer[]} capital
 # @return {Integer}
 def find_maximized_capital(k, w, profits, capital)
-  require 'pry'; binding.pry
+    length = profits.length
+    cp_array = profits.map.with_index{ |ele, index| [ele, capital[index]]}
+    cp_array.sort_by!{ |e| -e.first }
+    total_capital = w
+    tasks = 0
+    return total_capital if total_capital < capital.min
+
+    while tasks < k && cp_array.any?
+        no_task_performed = true
+       (0...(length - tasks)).each do |i|
+            if cp_array[i].last <= total_capital
+                total_capital += cp_array[i].first
+                cp_array.delete_at(i)
+                tasks += 1
+                no_task_performed = false
+                break
+            end
+       end
+       break if no_task_performed
+    end
+    total_capital
 end
 
 p find_maximized_capital(2, 0, [1,2,3], [0,1,1]) # 4
-# p find_maximized_capital(3, 0, [1,2,3], [0,1,2]) # 6
+p find_maximized_capital(3, 0, [1,2,3], [0,1,2]) # 6
