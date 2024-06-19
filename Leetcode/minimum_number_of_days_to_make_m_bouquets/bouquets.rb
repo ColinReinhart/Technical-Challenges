cb864323ab71212841aa6711082ce76c9fc379f8
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-19 13:53:00 -0600

    Setup Bouquets

---
+++ Leetcode/minimum_number_of_days_to_make_m_bouquets/bouquets.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} bloom_day
+# @param {Integer} m
+# @param {Integer} k
+# @return {Integer}
+def min_days(bloom_day, m, k)
+  require 'pry'; binding.pry
+end
+
+p min_days([1,10,3,10,2], 3, 1) # 3
+# p min_days([1,10,3,10,2], 3, 2) # -1
+# p min_days([7,7,7,7,12,7,7], 2, 3)  # 12
