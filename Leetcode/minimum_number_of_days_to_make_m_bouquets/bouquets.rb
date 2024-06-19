60dc1f2881c0418f166d42c80bc1daa23c77e818
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-19 14:33:57 -0600

    Passing all testcases

--- Leetcode/minimum_number_of_days_to_make_m_bouquets/bouquets.rb
+++ Leetcode/minimum_number_of_days_to_make_m_bouquets/bouquets.rb
@@ -3,9 +3,43 @@
 # @param {Integer} k
 # @return {Integer}
 def min_days(bloom_day, m, k)
-  require 'pry'; binding.pry
+  return -1 if bloom_day.length < m * k
+
+  left = 1
+  right = bloom_day.max
+
+  def can_make_bouquets?(bloom_day, m, k, mid)
+    count = 0
+    flowers = 0
+
+    bloom_day.each do |day|
+      if day <= mid
+        flowers += 1
+        if flowers == k
+          count += 1
+          flowers = 0
+        end
+      else
+        flowers = 0
+      end
+    end
+
+    count >= m
+  end
+
+  while left < right
+    mid = (left + right) / 2
+    if can_make_bouquets?(bloom_day, m, k, mid)
+      right = mid
+    else
+      left = mid + 1
+    end
+  end
+
+  left
 end
 
+
 p min_days([1,10,3,10,2], 3, 1) # 3
-# p min_days([1,10,3,10,2], 3, 2) # -1
-# p min_days([7,7,7,7,12,7,7], 2, 3)  # 12
+p min_days([1,10,3,10,2], 3, 2) # -1
+p min_days([7,7,7,7,12,7,7], 2, 3)  # 12
