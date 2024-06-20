a42812fe32366ddc8975442225a3612d904332ba
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-20 15:06:05 -0600

    Passing all testcases

--- Leetcode/magnetice_force_between_two_balls/magnetic_force_between_two_balls.rb
+++ Leetcode/magnetice_force_between_two_balls/magnetic_force_between_two_balls.rb
@@ -1,9 +1,35 @@
-# @param {Integer[]} position
-# @param {Integer} m
-# @return {Integer}
 def max_distance(position, m)
-  require 'pry'; binding.pry
+  position.sort!
+
+  left = 1
+  right = position.last - position.first
+
+  def can_place_balls?(position, mid, m)
+    count = 1
+    last_position = position[0]
+
+    position.each do |p|
+      if p - last_position >= mid
+        count += 1
+        last_position = p
+        return true if count >= m
+      end
+    end
+
+    false
+  end
+
+  while left < right
+    mid = (left + right + 1) / 2
+    if can_place_balls?(position, mid, m)
+      left = mid
+    else
+      right = mid - 1
+    end
+  end
+
+  left
 end
 
-p max_distance([1,2,3,4,7], 3) # 3
-# p max_distance([5,4,3,2,1,1000000000], 2) # 999999999
+p max_distance([1, 2, 8, 4, 9], 3) #3
+p max_distance([5, 4, 3, 2, 1, 1000000000], 2) #999999999
