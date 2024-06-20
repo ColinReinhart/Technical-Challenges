23a219d14b420515ec00942b8e440aff0a43d2c3
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-20 10:37:35 -0600

    Setup Magnetic Force Between Two Balls

---
+++ Leetcode/magnetice_force_between_two_balls/magnetic_force_between_two_balls.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} position
+# @param {Integer} m
+# @return {Integer}
+def max_distance(position, m)
+  require 'pry'; binding.pry
+end
+
+p max_distance([1,2,3,4,7], 3) # 3
+# p max_distance([5,4,3,2,1,1000000000], 2) # 999999999
