6119fea40a5f03d34d17c3066b87f18a1f7fba10
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-20 15:32:20 -0600

    Setup Roman to Integer

---
+++ Leetcode/roman_to_integer/roman_to_integer.rb
@@ -0,0 +1,11 @@
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+  require 'pry'; binding.pry
+end
+
+p roman_to_int('III')
+# p roman_to_int('IV')
+# p roman_to_int('IX')
+# p roman_to_int('LVIII')
+# p roman_to_int('MCMXCIV')
