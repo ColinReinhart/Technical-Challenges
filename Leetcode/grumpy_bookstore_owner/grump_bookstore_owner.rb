5116b0a624872c3ce88afa1c1af07d87b7045ec4
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-21 15:24:56 -0600

    Passing 73 of 78 testcases, time limit exceeded

--- Leetcode/grumpy_bookstore_owner/grump_bookstore_owner.rb
+++ Leetcode/grumpy_bookstore_owner/grump_bookstore_owner.rb
@@ -4,35 +4,29 @@
 # @return {Integer}
 def max_satisfied(customers, grumpy, minutes)
   max = customers.each_with_index.inject(0) { |sum, (c, i)| grumpy[i] == 0 ? sum + c : sum }
+  increase = 0
 
   left = 0
   right = minutes - 1
 
-  def calc_satisfaction(customers, grumpy)
-    wip = 0
-    customers.each_with_index do |c,i|
-      if grumpy[i] == 0
-        wip += c
+  until right == customers.length
+    inc = 0
+
+    customers[left..right].each_with_index do |c,i|
+      if grumpy[left..right][i] == 1
+        inc += c
       end
     end
-    wip
-  end
 
-  def ungrump(cust, grump, max, left, right)
-    temp = grump
-    temp[left..right] = temp[left..right].map { |x| x = 0 }
-    calc_satisfaction(cust, temp)
-  end
-
-  until right == customers.length + 1
-    if customers.ungrump(customers, grumpy, max, left, right) > max
-      max = customers.ungrump(customers, grumpy, max, left, right)
+    if inc > increase
+      increase = inc
     end
+
     left += 1
     right += 1
   end
-  require 'pry'; binding.pry
-  max
+  max + increase
 end
 
 p max_satisfied([1,0,1,2,1,1,7,5], [0,1,0,1,0,1,0,1], 3) # 16
+p max_satisfied([1], [0], 1) # 1
