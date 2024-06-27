724bddd7addebd64d1839ab2c4d1e37074885a1f
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-27 10:03:27 -0600

    Setup Find Center of Star Graph

---
+++ Leetcode/find_center_of_star_graph/find_center_of_star_graph.rb
@@ -0,0 +1,8 @@
+# @param {Integer[][]} edges
+# @return {Integer}
+def find_center(edges)
+  require 'pry'; binding.pry
+end
+
+ p find_center([[1,2],[2,3],[4,2]])
+#  p find_center([[1,2],[5,1],[1,3],[1,4]])
