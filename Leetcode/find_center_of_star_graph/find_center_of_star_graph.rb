9240e4c81c58ae311d9e3bfec99b3438f1be7c10
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-27 15:07:46 -0600

    Passing all test cases

--- Leetcode/find_center_of_star_graph/find_center_of_star_graph.rb
+++ Leetcode/find_center_of_star_graph/find_center_of_star_graph.rb
@@ -1,8 +1,11 @@
 # @param {Integer[][]} edges
 # @return {Integer}
 def find_center(edges)
-  require 'pry'; binding.pry
+  hash = Hash.new(0)
+  edges.flatten.each { |x| hash[x] += 1 }
+
+  hash.key(edges.count)
 end
 
- p find_center([[1,2],[2,3],[4,2]])
-#  p find_center([[1,2],[5,1],[1,3],[1,4]])
+ p find_center([[1,2],[2,3],[4,2]]) #2
+ p find_center([[1,2],[5,1],[1,3],[1,4]]) #1
