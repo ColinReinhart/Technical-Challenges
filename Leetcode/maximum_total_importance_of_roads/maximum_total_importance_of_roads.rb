8b13dff087af1c3f5d8a5bcff8862a5706ce643e
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-28 09:33:07 -0600

    Setup Maximum Total Importance of Roads

---
+++ Leetcode/maximum_total_importance_of_roads/maximum_total_importance_of_roads.rb
@@ -0,0 +1,9 @@
+# @param {Integer} n
+# @param {Integer[][]} roads
+# @return {Integer}
+def maximum_importance(n, roads)
+  require 'pry'; binding.pry
+end
+
+p maximum_importance(5, [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]]) #43
+# p maximum_importance(5, [[0,3],[2,4],[1,3]]) #20
