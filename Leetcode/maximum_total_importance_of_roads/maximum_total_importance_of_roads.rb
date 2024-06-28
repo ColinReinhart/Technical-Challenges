30c0113429c1c75af379f1078ccb64763dbc2664
Colin Reinhart <colinbreinhart@gmail.com>
2024-06-28 10:55:13 -0600

    Passing all testcases

--- Leetcode/maximum_total_importance_of_roads/maximum_total_importance_of_roads.rb
+++ Leetcode/maximum_total_importance_of_roads/maximum_total_importance_of_roads.rb
@@ -2,8 +2,23 @@
 # @param {Integer[][]} roads
 # @return {Integer}
 def maximum_importance(n, roads)
-  require 'pry'; binding.pry
+  max_value = 0
+  hash = Hash.new(0)
+  roads.flatten.each { |x| hash[x] += 1 }
+  sorted = hash.sort_by { |k,v| v }
+
+  city_value = Hash.new(0)
+  sorted.reverse.each do |k, v|
+    city_value[k] += n
+    n -= 1
+  end
+
+  roads.each do |k,v|
+    max_value += city_value[k] + city_value[v]
+  end
+
+  max_value
 end
 
 p maximum_importance(5, [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]]) #43
-# p maximum_importance(5, [[0,3],[2,4],[1,3]]) #20
+p maximum_importance(5, [[0,3],[2,4],[1,3]]) #20
