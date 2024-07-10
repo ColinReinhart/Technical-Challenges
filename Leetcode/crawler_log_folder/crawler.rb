d802f29fbc90dd528651c2dcd780724b71c203de
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-10 10:38:59 -0600

    Passing all testcases

--- Leetcode/crawler_log_folder/crawler.rb
+++ Leetcode/crawler_log_folder/crawler.rb
@@ -1,8 +1,24 @@
 # @param {String[]} logs
 # @return {Integer}
 def min_operations(logs)
-  require 'pry'; binding.pry
+  until logs[0] != "../"
+    logs.shift
+  end
+  count = 0
+  logs.each do |log|
+    if log == "../" && count > 0
+      count -= 1
+    elsif log == "../" && count == 0
+      count += 0
+    elsif log == "./"
+      count += 0
+    else
+      count += 1
+    end
+  end
+  count
 end
 
 p min_operations(["d1/","d2/","../","d21/","./"])
-# p min_operations(["d1/","d2/","./","d3/","../","d31/"])
+p min_operations(["d1/","d2/","./","d3/","../","d31/"])
+p min_operations(["d1/","../","../","../"])
