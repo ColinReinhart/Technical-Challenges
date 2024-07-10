84446c9b16351d2be2dfb6fb8baa9f7b5466a40f
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-10 10:31:41 -0600

    Setup Crawler Log Folder

---
+++ Leetcode/crawler_log_folder/crawler.rb
@@ -0,0 +1,8 @@
+# @param {String[]} logs
+# @return {Integer}
+def min_operations(logs)
+  require 'pry'; binding.pry
+end
+
+p min_operations(["d1/","d2/","../","d21/","./"])
+# p min_operations(["d1/","d2/","./","d3/","../","d31/"])
