a220449c3f707bc7abbb2d52ecb5aafce8404a29
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-11 10:27:13 -0600

    Setup Reverse Substring Between Parentheses

---
+++ Leetcode/reverse_substrings_between_each_pair_of_parentheses/reverse_substrings_between_parentheses.rb
@@ -0,0 +1,9 @@
+# @param {String} s
+# @return {String}
+def reverse_parentheses(s)
+  require 'pry'; binding.pry
+end
+
+p reverse_parentheses("(abcd)")
+# p reverse_parentheses("(u(love)i)")
+# p reverse_parentheses("(ed(et(oc))el)")
