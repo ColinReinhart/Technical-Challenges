914feee3e4c0469a110aef3ed9f4d4a9ac39e215
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-11 11:53:28 -0600

    Markdown and refactor

--- Leetcode/reverse_substrings_between_each_pair_of_parentheses/reverse_substrings_between_parentheses.rb
+++ Leetcode/reverse_substrings_between_each_pair_of_parentheses/reverse_substrings_between_parentheses.rb
@@ -1,30 +1,21 @@
 # @param {String} s
 # @return {String}
 def reverse_parentheses(s)
-  @wip = s.split('')
-  @left = 0
-  @right = 0
+  wip = s.chars
 
-  def put_my_thang_down_flip_it_and_reverse_it(s)
-    @wip.each_with_index do |x,i|
-      if x == ('(')
-        @left = i
-      elsif x == (')')
-        @right = i
-        break
-      end
-    end
-
-    @wip[@left..@right] = @wip[@left+1..@right-1].reverse
+  def put_my_thang_down_flip_it_and_reverse_it(wip)
+    left = wip.rindex('(')
+    right = wip.index(')')
+    wip[left..right] = wip[left+1...right].reverse
   end
 
-  while @wip.include?('(')
-    put_my_thang_down_flip_it_and_reverse_it(@wip)
+  while wip.include?('(')
+    put_my_thang_down_flip_it_and_reverse_it(wip)
   end
 
-  @wip.join
+  wip.join
 end
 
-# p reverse_parentheses("(abcd)")
+p reverse_parentheses("(abcd)")
 p reverse_parentheses("(u(love)i)")
-# p reverse_parentheses("(ed(et(oc))el)")
+p reverse_parentheses("(ed(et(oc))el)")
