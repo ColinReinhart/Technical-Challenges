21c5f715d3f800fcb3a50a022b7384e94336f843
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-11 11:39:17 -0600

    Passing all testcases

--- Leetcode/reverse_substrings_between_each_pair_of_parentheses/reverse_substrings_between_parentheses.rb
+++ Leetcode/reverse_substrings_between_each_pair_of_parentheses/reverse_substrings_between_parentheses.rb
@@ -1,9 +1,30 @@
 # @param {String} s
 # @return {String}
 def reverse_parentheses(s)
-  require 'pry'; binding.pry
+  @wip = s.split('')
+  @left = 0
+  @right = 0
+
+  def put_my_thang_down_flip_it_and_reverse_it(s)
+    @wip.each_with_index do |x,i|
+      if x == ('(')
+        @left = i
+      elsif x == (')')
+        @right = i
+        break
+      end
+    end
+
+    @wip[@left..@right] = @wip[@left+1..@right-1].reverse
+  end
+
+  while @wip.include?('(')
+    put_my_thang_down_flip_it_and_reverse_it(@wip)
+  end
+
+  @wip.join
 end
 
-p reverse_parentheses("(abcd)")
-# p reverse_parentheses("(u(love)i)")
+# p reverse_parentheses("(abcd)")
+p reverse_parentheses("(u(love)i)")
 # p reverse_parentheses("(ed(et(oc))el)")
