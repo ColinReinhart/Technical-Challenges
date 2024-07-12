8b4926f57f2eae5728ace4bc9d8c28b2dfc7434c
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-12 14:55:59 -0600

    Passing base testcases

--- Leetcode/maximum_score_from_removing_substring/maximum_score.rb
+++ Leetcode/maximum_score_from_removing_substring/maximum_score.rb
@@ -3,8 +3,52 @@
 # @param {Integer} y
 # @return {Integer}
 def maximum_gain(s, x, y)
-    require 'pry'; binding.pry
+  score = 0
+  left = 0
+  right = 1
+
+  def score_ab(s, x, left, right)
+    tally = 0
+    until s[right] == nil
+      if s[left..right] == "ab"
+        tally += x
+        s[left..right] = ""
+        left = 0
+        right = 1
+      else
+        left += 1
+        right += 1
+      end
+    end
+    tally
+  end
+
+  def score_ba(s,y, left, right)
+    tally = 0
+    until s[right] == nil
+      if s[left..right] == "ba"
+        tally += y
+        s[left..right] = ""
+        left = 0
+        right = 1
+        else
+          left += 1
+          right += 1
+        end
+    end
+    tally
+  end
+
+  if x > y
+    score += score_ab(s, x, left, right)
+    score += score_ba(s, y, left, right)
+  else
+    score += score_ba(s, y, left, right)
+    score += score_ab(s, x, left, right)
+  end
+
+  score
 end
 
-p maximum_gain("cdbcbbaaabab", 4, 5)
-# p maximum_gain("aabbaaxybbaabb", 5, 4)
+p maximum_gain("cdbcbbaaabab", 4, 5) #19
+p maximum_gain("aabbaaxybbaabb", 5, 4) #20
