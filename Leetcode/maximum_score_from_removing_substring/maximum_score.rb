e6ec233136bb543be406dc42ef84be88ddd5665f
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-12 09:10:33 -0600

    Setup Maximum Score From Removing Substrings

---
+++ Leetcode/maximum_score_from_removing_substring/maximum_score.rb
@@ -0,0 +1,10 @@
+# @param {String} s
+# @param {Integer} x
+# @param {Integer} y
+# @return {Integer}
+def maximum_gain(s, x, y)
+    require 'pry'; binding.pry
+end
+
+p maximum_gain("cdbcbbaaabab", 4, 5)
+# p maximum_gain("aabbaaxybbaabb", 5, 4)
