28804c03fb051555054bbfba05995204e0794e73
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-19 10:33:43 -0600

    Refactor

--- Leetcode/lucky_numbers_in_a_matrix/lucky_numbers_in_a_matric.rb
+++ Leetcode/lucky_numbers_in_a_matrix/lucky_numbers_in_a_matric.rb
@@ -1,29 +1,10 @@
 # @param {Integer[][]} matrix
 # @return {Integer[]}
-def lucky_numbers (matrix)
-  lucky = []
-  min_in_row = []
-  max_in_column = []
-  column = 0
+def lucky_numbers(matrix)
+  min_in_row = matrix.map(&:min)
+  max_in_column = matrix.transpose.map(&:max)
 
-  matrix.each { |arr| min_in_row << arr.min}
-
-  until matrix[0][column] == nil
-    wip = []
-    matrix.each do |arr|
-      wip << arr[column]
-    end
-    max_in_column << wip.max
-    column += 1
-  end
-
-  min_in_row.each do |min|
-    if max_in_column.include?(min)
-      lucky << min
-    end
-  end
-
-  lucky
+  min_in_row.select { |min| max_in_column.include?(min) }
 end
 
 p lucky_numbers([[3,7,8],[9,11,13],[15,16,17]]) #15
