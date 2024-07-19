bdf0c1595c3651cfd0c3ff884bcbb5f2837864ca
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-19 09:39:40 -0600

    Setup Lucky Numbers in a Matrix problem

---
+++ Leetcode/lucky_numbers_in_a_matrix/lucky_numbers_in_a_matric.rb
@@ -0,0 +1,9 @@
+# @param {Integer[][]} matrix
+# @return {Integer[]}
+def lucky_numbers (matrix)
+  require 'pry'; binding.pry
+end
+
+p lucky_numbers([[3,7,8],[9,11,13],[15,16,17]]) #15
+# p lucky_numbers([[1,10,4,2],[9,3,8,7],[15,16,17,12]]) #12
+# p lucky_numbers([[7,8],[1,2]]) #7
