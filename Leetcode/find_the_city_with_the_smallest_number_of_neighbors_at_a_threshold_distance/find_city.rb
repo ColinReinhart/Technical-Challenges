d2de9ff20af8f157e5fe4ed1dc944ad31d663f97
Colin Reinhart <colinbreinhart@gmail.com>
2024-07-26 10:57:09 -0600

    Setup problem with name that is too long to write right now

---
+++ Leetcode/find_the_city_with_the_smallest_number_of_neighbors_at_a_threshold_distance/find_city.rb
@@ -0,0 +1,10 @@
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @param {Integer} distance_threshold
+# @return {Integer}
+def find_the_city(n, edges, distance_threshold)
+    require 'pry'; binding.pry
+end
+
+p find_the_city(4, [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], 4)
+# p find_the_city(5, [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], 2)
