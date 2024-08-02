7f1c638c9efe7353f09c1f724c1e1bc3c38eea2a
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-02 10:14:39 -0600

    Setup Number of Senior Citizens

---
+++ Leetcode/number_of_senior_citizens/number_of_senior_citizens.rb
@@ -0,0 +1,8 @@
+# @param {String[]} details
+# @return {Integer}
+def count_seniors(details)
+  require 'pry'; binding.pry
+end
+
+count_seniors(["7868190130M7522","5303914400F9211","9273338290F4010"])
+# count_seniors(["1313579440F2036","2921522980M5644"])
