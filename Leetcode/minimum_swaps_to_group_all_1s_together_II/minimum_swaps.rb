fef85b97d7637d3eb4d5239bb88155c378174bb0
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-02 09:24:15 -0600

    Setup Minimum Swaps Problem

---
+++ Leetcode/minimum_swaps_to_group_all_1s_together_II/minimum_swaps.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def min_swaps(nums)
+    require 'pry'; binding.pry
+end
+
+p min_swaps([0,1,0,1,1,0,0])
+# p min_swaps([0,1,1,1,0,0,1,1,0])
+# p min_swaps([1,1,0,0,1])
