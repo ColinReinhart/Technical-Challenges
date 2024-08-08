b930f766dd505414f41d274170bfeac3e08c4403
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-08 10:46:07 -0600

    Setup Spiral Matrix III

---
+++ Leetcode/spiral_matrix_III/spiral_matrix_III.rb
@@ -0,0 +1,11 @@
+# @param {Integer} rows
+# @param {Integer} cols
+# @param {Integer} r_start
+# @param {Integer} c_start
+# @return {Integer[][]}
+def spiral_matrix_iii(rows, cols, r_start, c_start)
+    require 'pry'; binding.pry
+end
+
+p spiral_matrix_iii(1, 2, 0, 0)
+# p spiral_matrix_iii(5, 6, 1, 4)
