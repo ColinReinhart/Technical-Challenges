036fdba9895d3e25ff32cc7bc3d5babe9bac7c99
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-15 12:52:13 -0600

    Setup Lemonade Change

---
+++ Leetcode/lemonade_change/lemonade_change.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} bills
+# @return {Boolean}
+def lemonade_change(bills)
+    require 'pry'; binding.pry
+end
+
+p lemonade_change([5,5,5,10,20]) #true
+# p lemonade_change([5,5,10,10,20]) #false
