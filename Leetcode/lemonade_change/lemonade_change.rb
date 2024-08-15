5b8e0823ffd2f9906c680c339d76268dcd6f0f05
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-15 15:01:52 -0600

    Passing all testcases

--- Leetcode/lemonade_change/lemonade_change.rb
+++ Leetcode/lemonade_change/lemonade_change.rb
@@ -1,8 +1,37 @@
 # @param {Integer[]} bills
 # @return {Boolean}
 def lemonade_change(bills)
-    require 'pry'; binding.pry
+  till = []
+  bills.each do |bill|
+    if bill == 5
+      till << bill
+    elsif bill == 10
+      if till.include?(5)
+        till << bill
+        till.delete_at(till.index(5))
+      else
+        return false
+      end
+    elsif bill == 20
+      if till.include?(10)
+        if till.include?(5)
+          till << bill
+          till.delete_at(till.index(5))
+          till.delete_at(till.index(10))
+        else
+          return false
+        end
+      elsif till.count(5) >= 3
+        till << bill
+        3.times { till.delete_at(till.index(5)) }
+      else
+        return false
+      end
+    end
+  end
+  return true
 end
 
-p lemonade_change([5,5,5,10,20]) #true
+# p lemonade_change([5,5,5,10,20]) #true
 # p lemonade_change([5,5,10,10,20]) #false
+p lemonade_change([5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5]) #true
