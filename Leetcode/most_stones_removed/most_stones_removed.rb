89f7bf9118b218c85c974c30d11b9b83546fd342
Colin Reinhart <colinbreinhart@gmail.com>
2024-08-29 15:07:50 -0600

    Setup Most Stones Removed Problem

---
+++ Leetcode/most_stones_removed/most_stones_removed.rb
@@ -0,0 +1,8 @@
+# @param {Integer[][]} stones
+# @return {Integer}
+def remove_stones(stones)
+    require 'pry'; binding.pry
+end
+
+p remove_stones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]])
+# p remove_stones([[0,0],[0,2],[1,1],[2,0],[2,2]])
