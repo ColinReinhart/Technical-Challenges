33f7fd0eca32c6e946d69d513f5f69d7b5cc26ec
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-06 15:25:53 -0600

    Should be passing all test cases

--- Leetcode/find_missing_observations/find_missing_observations.rb
+++ Leetcode/find_missing_observations/find_missing_observations.rb
@@ -3,7 +3,21 @@
 # @param {Integer} n
 # @return {Integer[]}
 def missing_rolls(rolls, mean, n)
-    require 'pry'; binding.pry
+  m = rolls.length
+  total_sum = mean * (n + m)
+  current_sum = rolls.sum
+  missing_sum = total_sum - current_sum
+
+  if missing_sum < n || missing_sum > 6 * n
+    return []
+  end
+
+  result = Array.new(n, missing_sum / n)
+  remainder = missing_sum % n
+
+  remainder.times { |i| result[i] += 1 }
+
+  result
 end
 
 p missing_rolls([3,2,4,3], 4, 2) #[6,6]
