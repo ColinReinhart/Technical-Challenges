d83fecb4724156ad072e6b461c4aa232c54df2ad
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-11 15:07:46 -0600

    Passing all testcases

--- Leetcode/delete_nodes_from_linked_list_present_in_array/delete_nodes.rb
+++ Leetcode/delete_nodes_from_linked_list_present_in_array/delete_nodes.rb
@@ -1,18 +1,64 @@
+require 'set'
 # Definition for singly-linked list.
-# class ListNode
-#     attr_accessor :val, :next
-#     def initialize(val = 0, _next = nil)
-#         @val = val
-#         @next = _next
-#     end
-# end
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
 # @param {Integer[]} nums
 # @param {ListNode} head
 # @return {ListNode}
 def modified_list(nums, head)
-  require 'pry'; binding.pry
+  # Use a set for O(1) lookup
+  num_set = nums.to_set
+
+  # Dummy node to handle edge cases
+  dummy = ListNode.new(0)
+  dummy.next = head
+  prev, current = dummy, head
+
+  # Traverse the linked list
+  while current
+    if num_set.include?(current.val)
+      # Skip the current node
+      prev.next = current.next
+    else
+      prev = current
+    end
+    current = current.next
+  end
+
+  # Return the modified list starting from dummy.next
+  dummy.next
 end
 
-p modified_list( [1,2,3], [1,2,3,4,5]) #[4, 5]
-# p modified_list( [1], [1,2,1,2,1,2]) #[2, 2, 2]
-# p modified_list( [5], [1,2,3,4]) #[1, 2, 3, 4]
+# Helper function to create a linked list from an array
+def create_linked_list(arr)
+  return nil if arr.empty?
+  head = ListNode.new(arr[0])
+  current = head
+  arr[1..-1].each do |val|
+    current.next = ListNode.new(val)
+    current = current.next
+  end
+  head
+end
+
+# Helper function to print a linked list
+def print_linked_list(head)
+  current = head
+  result = []
+  while current
+    result << current.val
+    current = current.next
+  end
+  p result
+end
+
+# Example usage
+head = create_linked_list([1, 2, 3, 4, 5])
+modified_head = modified_list([1, 2, 3], head)
+print_linked_list(modified_head) # Output: [4, 5]
