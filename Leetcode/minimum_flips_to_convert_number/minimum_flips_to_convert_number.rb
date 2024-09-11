68ac8353d46c2fbfaaa5766c0cdb4a34ea2c0834
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-11 15:19:57 -0600

    Setup Problem

---
+++ Leetcode/minimum_flips_to_convert_number/minimum_flips_to_convert_number.rb
@@ -0,0 +1,9 @@
+# @param {Integer} start
+# @param {Integer} goal
+# @return {Integer}
+def min_bit_flips(start, goal)
+  require 'pry'; binding.pry
+end
+
+p min_bit_flips(10, 7) #3
+p min_bit_flips(3,4) #3
