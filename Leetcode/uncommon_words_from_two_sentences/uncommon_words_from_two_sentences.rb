ff734322a3207e8b59fc6616ab8db02edec6ff5f
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-17 12:02:43 -0600

    Passing all testcases

--- Leetcode/uncommon_words_from_two_sentences/uncommon_words_from_two_sentences.rb
+++ Leetcode/uncommon_words_from_two_sentences/uncommon_words_from_two_sentences.rb
@@ -2,8 +2,13 @@
 # @param {String} s2
 # @return {String[]}
 def uncommon_from_sentences(s1, s2)
-    require 'pry'; binding.pry
+  hash = Hash.new(0)
+
+  s1.split(' ').each { |word| hash[word] += 1 }
+  s2.split(' ').each { |word| hash[word] += 1 }
+
+  hash.select { |key, value| value == 1 }.keys
 end
 
 p uncommon_from_sentences("this apple is sweet", "this apple is sour") #["sweet","sour"]
-# p uncommon_from_sentences("apple apple", "banana") #["banana"]
+p uncommon_from_sentences("apple apple", "banana") #["banana"]
