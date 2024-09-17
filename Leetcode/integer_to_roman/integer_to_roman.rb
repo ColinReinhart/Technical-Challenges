0499c3115e73a6be6fac0ae895159fadff33eb18
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-17 14:58:04 -0600

    Passing all testcases

--- Leetcode/integer_to_roman/integer_to_roman.rb
+++ Leetcode/integer_to_roman/integer_to_roman.rb
@@ -1,9 +1,23 @@
 # @param {Integer} num
 # @return {String}
 def int_to_roman(num)
-  require 'pry'; binding.pry
+  roman_numerals = [
+    [1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+    [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'],
+    [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+  ]
+
+  result = ""
+  roman_numerals.each do |value, symbol|
+    while num >= value
+      result << symbol
+      num -= value
+    end
+  end
+
+  result
 end
 
-p int_to_roman(3749)
-p int_to_roman(58)
-p int_to_roman(1994)
+p int_to_roman(3749) #"MMMDCCXLIX"
+p int_to_roman(58) #"LVIII"
+p int_to_roman(1994) #"MCMXCIV"
