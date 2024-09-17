6b20379f90cb2d8d37a1f4655197840db2a9c4f6
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-17 11:19:56 -0600

    Setup Problem

---
+++ Leetcode/uncommon_words_from_two_sentences/uncommon_words_from_two_sentences.rb
@@ -0,0 +1,9 @@
+# @param {String} s1
+# @param {String} s2
+# @return {String[]}
+def uncommon_from_sentences(s1, s2)
+    require 'pry'; binding.pry
+end
+
+p uncommon_from_sentences("this apple is sweet", "this apple is sour") #["sweet","sour"]
+# p uncommon_from_sentences("apple apple", "banana") #["banana"]
