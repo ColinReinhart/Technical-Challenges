77dcd05a7ad61efbb097c377476ed63bfbe5d72e
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-18 14:38:54 -0600

    Setup Largest Number problem

---
+++ Leetcode/largest_number/largest_number.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {String}
+def largest_number(nums)
+  require 'pry'; binding.pry
+end
+
+p largest_number([10,2]) #"210"
+# p largest_number([3,30,34,5,9]) #"9534330"
