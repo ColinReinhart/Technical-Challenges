2818331f8b1e296f10ffd8d0fe96e00c2b2900ae
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-18 15:32:13 -0600

    Passing all testcases

--- Leetcode/largest_number/largest_number.rb
+++ Leetcode/largest_number/largest_number.rb
@@ -1,8 +1,14 @@
 # @param {Integer[]} nums
 # @return {String}
 def largest_number(nums)
-  require 'pry'; binding.pry
+  nums = nums.map(&:to_s)
+
+  nums.sort! { |a, b| (b + a) <=> (a + b) }
+
+  result = nums.join
+
+  result[0] == '0' ? '0' : result
 end
 
 p largest_number([10,2]) #"210"
-# p largest_number([3,30,34,5,9]) #"9534330"
+p largest_number([3,30,34,5,9,109]) #"9534330"
