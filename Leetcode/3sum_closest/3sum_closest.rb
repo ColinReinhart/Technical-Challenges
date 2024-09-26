817f4bb141f4f2585a297eff79448dd612a09781
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-26 15:52:07 -0600

    Passing base testcases, but I think I missunderstood the asignment, I was just trying to find the three closest numbers and add them together, but what I need to return is the three numbers whose sum is closest to target and return the sume that gets the closest

--- Leetcode/3sum_closest/3sum_closest.rb
+++ Leetcode/3sum_closest/3sum_closest.rb
@@ -2,8 +2,19 @@
 # @param {Integer} target
 # @return {Integer}
 def three_sum_closest(nums, target)
-  require 'pry'; binding.pry
+  arr = []
+  sum = 0
+
+  nums.each { |num| arr <<  [(num - target).abs, num] }
+
+  arr.sort!
+
+  3.times do
+    sum += arr.shift[1]
+  end
+
+  sum
 end
 
-p three_sum_closest([-1,2,1,-4], 1)
-# p three_sum_closest([0,0,0], 1)
+p three_sum_closest([-1,2,1,-4], 1) #2
+p three_sum_closest([0,0,0], 1) #0
