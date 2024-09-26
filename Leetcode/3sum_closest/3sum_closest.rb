70e0394d9166b6cbbb5b4dbea07b79f8a6c5413d
Colin Reinhart <colinbreinhart@gmail.com>
2024-09-26 14:44:30 -0600

    Setup Problem

---
+++ Leetcode/3sum_closest/3sum_closest.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def three_sum_closest(nums, target)
+  require 'pry'; binding.pry
+end
+
+p three_sum_closest([-1,2,1,-4], 1)
+# p three_sum_closest([0,0,0], 1)
