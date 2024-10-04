eb880095eee56edfad6157c7092d9f418a204d62
Colin Reinhart <colinbreinhart@gmail.com>
2024-10-04 10:17:15 -0600

    Setup problem

---
+++ Leetcode/divide_players_into_teams_of_equal_skill/divide_players.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} skill
+# @return {Integer}
+def divide_players(skill)
+  require 'pry'; binding.pry
+end
+
+p divide_players([3,2,5,1,3,4]) #22
+# p divide_players([3,4]) #12
+# p divide_players([1,1,2,3]) #-1
