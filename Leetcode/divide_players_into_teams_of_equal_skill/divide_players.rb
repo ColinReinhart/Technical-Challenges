3c2ce2f48e7248b853aaed2dafa865fea6f3b2d3
Colin Reinhart <colinbreinhart@gmail.com>
2024-10-04 10:44:38 -0600

    Passing testcases 1-68, time limit exceeded on 69

--- Leetcode/divide_players_into_teams_of_equal_skill/divide_players.rb
+++ Leetcode/divide_players_into_teams_of_equal_skill/divide_players.rb
@@ -1,9 +1,23 @@
 # @param {Integer[]} skill
 # @return {Integer}
 def divide_players(skill)
-  require 'pry'; binding.pry
+  arr = []
+  target = skill.max + skill.min
+
+  skill.sort!
+  until skill == []
+    if skill.max + skill.min != target
+      return -1
+    else
+      arr << [skill.shift, skill.pop]
+    end
+  end
+
+  answer = 0
+  arr.each { |x, y| answer += x * y }
+  answer
 end
 
 p divide_players([3,2,5,1,3,4]) #22
-# p divide_players([3,4]) #12
-# p divide_players([1,1,2,3]) #-1
+p divide_players([3,4]) #12
+p divide_players([1,1,2,3]) #-1
