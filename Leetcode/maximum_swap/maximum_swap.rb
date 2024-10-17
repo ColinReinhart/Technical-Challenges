847b5369a47aa40b8739d8848256ee5b8cda2dce
Colin Reinhart <colinbreinhart@gmail.com>
2024-10-17 09:58:13 -0600

    Passing base cases, poorly written instructions led me to believe I could only move numbers next to eachother but I can swap any two numbers.

--- Leetcode/maximum_swap/maximum_swap.rb
+++ Leetcode/maximum_swap/maximum_swap.rb
@@ -1,8 +1,20 @@
 # @param {Integer} num
 # @return {Integer}
 def maximum_swap(num)
-  require 'pry'; binding.pry
+  num = num.to_s.split('').map { |n| n.to_i }
+
+  num.each_with_index do |n,i|
+    if num[i+1] == nil
+      return num.join('').to_i
+    elsif n < num[i+1]
+      x = n
+      y = num[i+1]
+      num[i] = y
+      num[i+1] = x
+      return num.join('').to_i
+    end
+  end
 end
 
 p maximum_swap(2736) # 7236
-# p maximum_swap(9973) # 9973
+p maximum_swap(9973) # 9973
