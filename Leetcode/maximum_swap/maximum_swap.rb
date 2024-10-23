6e9c5fc3c5aa6879327bdf2e914341bf12e8dad3
Colin Reinhart <colinbreinhart@gmail.com>
2024-10-23 14:33:48 -0600

    Passing all testcases

--- Leetcode/maximum_swap/maximum_swap.rb
+++ Leetcode/maximum_swap/maximum_swap.rb
@@ -1,20 +1,30 @@
 # @param {Integer} num
 # @return {Integer}
 def maximum_swap(num)
-  num = num.to_s.split('').map { |n| n.to_i }
+  digits = num.to_s.chars.map(&:to_i)
+  n = digits.size
 
-  num.each_with_index do |n,i|
-    if num[i+1] == nil
-      return num.join('').to_i
-    elsif n < num[i+1]
-      x = n
-      y = num[i+1]
-      num[i] = y
-      num[i+1] = x
-      return num.join('').to_i
+  max_idx = Array.new(n)
+  max_idx[n-1] = n-1
+
+  (n-2).downto(0) do |i|
+    if digits[i] > digits[max_idx[i+1]]
+      max_idx[i] = i
+    else
+      max_idx[i] = max_idx[i+1]
     end
   end
+
+  (0...n).each do |i|
+    if digits[i] < digits[max_idx[i]]
+      digits[i], digits[max_idx[i]] = digits[max_idx[i]], digits[i]
+      break
+    end
+  end
+
+  digits.join.to_i
 end
 
-p maximum_swap(2736) # 7236
-p maximum_swap(9973) # 9973
+p maximum_swap(2736)  # 7236
+p maximum_swap(9973)  # 9973
+p maximum_swap(98368) # 98863
