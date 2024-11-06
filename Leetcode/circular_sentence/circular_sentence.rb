729e9d5682c94ae49551a6d2a7a0ce731ecdc0aa
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-06 11:40:08 -0700

    Setup Problem

---
+++ Leetcode/circular_sentence/circular_sentence.rb
@@ -0,0 +1,9 @@
+# @param {String} sentence
+# @return {Boolean}
+def is_circular_sentence(sentence)
+    require 'pry'; binding.pry
+end
+
+p is_circular_sentence("leetcode exercises sound delightful") #true
+# p is_circular_sentence("eetcode") #true
+# p is_circular_sentence("Leetcode is cool") #false
