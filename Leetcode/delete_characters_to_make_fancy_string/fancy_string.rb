c00300952efd91b7713fc33591a94e4e6838ff8c
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-06 11:15:50 -0700

    Passing testcases 1-292, time limit exceeded

--- Leetcode/delete_characters_to_make_fancy_string/fancy_string.rb
+++ Leetcode/delete_characters_to_make_fancy_string/fancy_string.rb
@@ -1,9 +1,21 @@
 # @param {String} s
 # @return {String}
 def make_fancy_string(s)
-  require 'pry'; binding.pry
+  chars = s.chars
+  indices = []
+
+  chars.each_with_index do |c,i|
+    if c == chars[i+1]
+      if c == chars[i+2]
+        indices << i
+      end
+    end
+  end
+  indices.reverse.each { |x| chars.delete_at(x) }
+
+  chars.join('')
 end
 
 p make_fancy_string("leeetcode") # "leetcode"
-# p make_fancy_string("aaabaaaa") # "aabaa"
-# p make_fancy_string("aab") # "aab"
+p make_fancy_string("aaabaaaa") # "aabaa"
+p make_fancy_string("aab") # "aab"
