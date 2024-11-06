3564553c896ae3a869d730d40ba2d9f7fd6953e6
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-06 09:48:48 -0700

    Setup Problem

---
+++ Leetcode/delete_characters_to_make_fancy_string/fancy_string.rb
@@ -0,0 +1,9 @@
+# @param {String} s
+# @return {String}
+def make_fancy_string(s)
+  require 'pry'; binding.pry
+end
+
+p make_fancy_string("leeetcode") # "leetcode"
+# p make_fancy_string("aaabaaaa") # "aabaa"
+# p make_fancy_string("aab") # "aab"
