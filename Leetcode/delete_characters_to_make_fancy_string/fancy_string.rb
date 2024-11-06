cbb54eb3a672a8364c6a9cde0af97e24452548e9
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-06 11:23:32 -0700

    Passing all testcases

--- Leetcode/delete_characters_to_make_fancy_string/fancy_string.rb
+++ Leetcode/delete_characters_to_make_fancy_string/fancy_string.rb
@@ -1,19 +1,22 @@
 # @param {String} s
 # @return {String}
 def make_fancy_string(s)
-  chars = s.chars
-  indices = []
+  result = []
+  repeat_count = 0
+  previous_char = nil
 
-  chars.each_with_index do |c,i|
-    if c == chars[i+1]
-      if c == chars[i+2]
-        indices << i
-      end
+  s.each_char do |char|
+    if char == previous_char
+      repeat_count += 1
+    else
+      repeat_count = 1
+      previous_char = char
     end
-  end
-  indices.reverse.each { |x| chars.delete_at(x) }
 
-  chars.join('')
+    result << char if repeat_count < 3
+  end
+
+  result.join
 end
 
 p make_fancy_string("leeetcode") # "leetcode"
