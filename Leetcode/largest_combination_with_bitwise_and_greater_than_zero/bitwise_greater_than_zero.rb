f67344f75bca7881970983a0ab44af9d6daf8b61
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-07 15:15:40 -0700

    Passing all testcases

--- Leetcode/largest_combination_with_bitwise_and_greater_than_zero/bitwise_greater_than_zero.rb
+++ Leetcode/largest_combination_with_bitwise_and_greater_than_zero/bitwise_greater_than_zero.rb
@@ -1,7 +1,18 @@
 # @param {Integer[]} candidates
 # @return {Integer}
 def largest_combination(candidates)
-  require 'pry'; binding.pry
+  bit_count = Array.new(24, 0)
+
+  candidates.each do |num|
+    bit_position = 0
+    while num > 0
+      bit_count[bit_position] += 1 if (num & 1) == 1
+      num >>= 1
+      bit_position += 1
+    end
+  end
+
+  bit_count.max
 end
 
 p largest_combination([16,17,71,62,12,24,14]) #4
