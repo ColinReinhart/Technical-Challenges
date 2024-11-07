eb2e7f3a74e6cdc543d4952281f2b9b4d1e3b47e
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-07 15:04:13 -0700

    Setup Problem

---
+++ Leetcode/largest_combination_with_bitwise_and_greater_than_zero/bitwise_greater_than_zero.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} candidates
+# @return {Integer}
+def largest_combination(candidates)
+  require 'pry'; binding.pry
+end
+
+p largest_combination([16,17,71,62,12,24,14]) #4
+p largest_combination([8,8]) #2
