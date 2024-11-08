c2de6131691e9c6b747cf8ccd8c8803331ffbfcd
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-08 09:42:33 -0700

    Setup Problem

---
+++ Leetcode/maximum_xor_for_each_query/maximum_xor_for_each_query.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @param {Integer} maximum_bit
+# @return {Integer[]}
+def get_maximum_xor(nums, maximum_bit)
+    require 'pry'; binding.pry
+end
+
+p get_maximum_xor([0,1,1,3], 2) #[0,3,2,3]
+# p get_maximum_xor([2,3,4,7], 3) #[5,2,6,5]
