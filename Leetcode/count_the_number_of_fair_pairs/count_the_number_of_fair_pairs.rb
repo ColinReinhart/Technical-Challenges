ac96cad3e9965fb3471178e796f264459c8ee0a5
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-13 14:06:14 -0700

    Passing testcases 1-45, time limit exceeded on large dataset

--- Leetcode/count_the_number_of_fair_pairs/count_the_number_of_fair_pairs.rb
+++ Leetcode/count_the_number_of_fair_pairs/count_the_number_of_fair_pairs.rb
@@ -3,8 +3,24 @@
 # @param {Integer} upper
 # @return {Integer}
 def count_fair_pairs(nums, lower, upper)
-  require 'pry'; binding.pry
+  left = 0
+  right = 1
+  final = nums.length - 1
+  count = 0
+
+  until left == final
+    until right == nums.length
+      if nums[left] + nums[right] >= lower && nums[left] + nums[right] <= upper
+        count += 1
+      end
+      right += 1
+    end
+    left += 1
+    right = left +1
+  end
+
+  count
 end
 
-p count_fair_pairs([0,1,7,4,4,5], 3, 6)
-# p count_fair_pairs([1,7,9,2,5], 11, 11)
+p count_fair_pairs([0,1,7,4,4,5], 3, 6) #6
+p count_fair_pairs([1,7,9,2,5], 11, 11) #1
