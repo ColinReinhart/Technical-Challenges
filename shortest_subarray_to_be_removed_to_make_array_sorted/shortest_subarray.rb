ef1470929de62c3be6039e31c9c2375d47296200
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-15 14:56:04 -0700

    Setup problem

---
+++ shortest_subarray_to_be_removed_to_make_array_sorted/shortest_subarray.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} arr
+# @return {Integer}
+def find_length_of_shortest_subarray(arr)
+  require 'pry'; binding.pry
+end
+
+p find_length_of_shortest_subarray([1,2,3,10,4,2,3,5]) #3
+# p find_length_of_shortest_subarray([5,4,3,2,1]) #4
+# p find_length_of_shortest_subarray([1,2,3]) #0
