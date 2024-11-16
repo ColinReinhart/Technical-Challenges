57edb8a05dbfe089a414234716e97bcda0bd570b
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-16 13:33:33 -0700

    Setup problem

---
+++ Leetcode/find_the_power_of_k-size_subarrays_I/find_the_power.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer[]}
+def results_array(nums, k)
+    require 'pry'; binding.pry
+end
+
+p results_array([1,2,3,4,3,2,5], 3) #[3,4,-1,-1,-1]
+# p results_array([2,2,2,2,2], 4) #[-1,-1]
+# p results_array([3,2,3,2,3,2], 2) #[-1,3,-1,3,-1]
