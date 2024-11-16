d68f75b778bb175d8bcecad4fd84afe472c71fcf
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-16 13:54:34 -0700

    Solved for consecutive numbers issue, works for problems 1-848. Time Limited Exceeded with large dataset

--- Leetcode/find_the_power_of_k-size_subarrays_I/find_the_power.rb
+++ Leetcode/find_the_power_of_k-size_subarrays_I/find_the_power.rb
@@ -7,7 +7,7 @@ def results_array(nums, k)
   answers = []
 
   until right == nums.count
-    if nums[left..right] == nums[left..right].sort && nums[left..right] == nums[left..right].uniq
+    if nums[left..right] == (nums[left..right].min..nums[left..right].max).to_a.sort
       answers << nums[left..right].max
     else
       answers << -1
@@ -19,7 +19,7 @@ def results_array(nums, k)
   answers
 end
 
-# p results_array([1,2,3,4,3,2,5], 3) #[3,4,-1,-1,-1]
-# p results_array([2,2,2,2,2], 4) #[-1,-1]
-# p results_array([3,2,3,2,3,2], 2) #[-1,3,-1,3,-1]
+p results_array([1,2,3,4,3,2,5], 3) #[3,4,-1,-1,-1]
+p results_array([2,2,2,2,2], 4) #[-1,-1]
+p results_array([3,2,3,2,3,2], 2) #[-1,3,-1,3,-1]
 p results_array([1,3,4], 2) #[-1,4]
