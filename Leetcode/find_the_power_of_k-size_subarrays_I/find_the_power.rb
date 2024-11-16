9fc32b549bc57069b2a54147b4f3be2ba5baba44
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-16 13:44:32 -0700

    Passing testcases 1-419, I missed that the numbers needed to be consequtive

--- Leetcode/find_the_power_of_k-size_subarrays_I/find_the_power.rb
+++ Leetcode/find_the_power_of_k-size_subarrays_I/find_the_power.rb
@@ -2,9 +2,24 @@
 # @param {Integer} k
 # @return {Integer[]}
 def results_array(nums, k)
-    require 'pry'; binding.pry
+  left = 0
+  right = k - 1
+  answers = []
+
+  until right == nums.count
+    if nums[left..right] == nums[left..right].sort && nums[left..right] == nums[left..right].uniq
+      answers << nums[left..right].max
+    else
+      answers << -1
+    end
+    left += 1
+    right += 1
+  end
+
+  answers
 end
 
-p results_array([1,2,3,4,3,2,5], 3) #[3,4,-1,-1,-1]
+# p results_array([1,2,3,4,3,2,5], 3) #[3,4,-1,-1,-1]
 # p results_array([2,2,2,2,2], 4) #[-1,-1]
 # p results_array([3,2,3,2,3,2], 2) #[-1,3,-1,3,-1]
+p results_array([1,3,4], 2) #[-1,4]
