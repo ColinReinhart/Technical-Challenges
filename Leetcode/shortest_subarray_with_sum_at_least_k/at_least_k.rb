85b2ba5f64cf1bf361fd3001fff6dd70a718fd3e
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-17 13:52:42 -0700

    Passing all testcases

--- Leetcode/shortest_subarray_with_sum_at_least_k/at_least_k.rb
+++ Leetcode/shortest_subarray_with_sum_at_least_k/at_least_k.rb
@@ -2,7 +2,27 @@
 # @param {Integer} k
 # @return {Integer}
 def shortest_subarray(nums, k)
-    require 'pry'; binding.pry
+  n = nums.length
+  prefix_sum = [0]
+
+  nums.each { |num| prefix_sum << prefix_sum.last + num }
+
+  deque = []
+  min_length = Float::INFINITY
+
+  prefix_sum.each_with_index do |sum, i|
+    while !deque.empty? && sum - prefix_sum[deque.first] >= k
+      min_length = [min_length, i - deque.shift].min
+    end
+
+    while !deque.empty? && sum <= prefix_sum[deque.last]
+      deque.pop
+    end
+
+    deque << i
+  end
+
+  min_length == Float::INFINITY ? -1 : min_length
 end
 
 p shortest_subarray([1], 1) #1
