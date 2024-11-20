4686f04b5fd9900bfa57da26866f3d9bf084eac3
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-20 10:05:02 -0700

    Setup Problem

---
+++ Leetcode/defuse_the_bomb/defuse_the_bomb.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} code
+# @param {Integer} k
+# @return {Integer[]}
+def decrypt(code, k)
+  require 'pry'; binding.pry
+end
+
+p decrypt([5,7,1,4], 3) #[12,10,16,13]
+# p decrypt([1,2,3,4], 0) #[0,0,0,0]
+# p decrypt([2,4,9,3], -2) #[12,5,6,13]
