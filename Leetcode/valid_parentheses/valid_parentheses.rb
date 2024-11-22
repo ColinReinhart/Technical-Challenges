f8b8830ab023b9bbc604e2ade770b8560962cc73
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-22 15:38:04 -0700

    Passing all testcases

--- Leetcode/valid_parentheses/valid_parentheses.rb
+++ Leetcode/valid_parentheses/valid_parentheses.rb
@@ -1,7 +1,18 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
-  require 'pry'; binding.pry
+  stack = []
+  matching_bracket = { ')' => '(', '}' => '{', ']' => '[' }
+
+  s.each_char do |char|
+    if matching_bracket.values.include?(char)
+      stack.push(char) # Push open brackets
+    elsif matching_bracket.key?(char)
+      return false if stack.empty? || stack.pop != matching_bracket[char]
+    end
+  end
+
+  stack.empty?
 end
 
 p is_valid("()") #true
