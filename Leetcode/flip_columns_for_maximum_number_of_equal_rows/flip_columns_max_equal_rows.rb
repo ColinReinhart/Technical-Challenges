9a56bc53e08bdff0e62a8b4e8bb3d34271efe99d
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-22 09:55:20 -0700

    Setup problem

---
+++ Leetcode/flip_columns_for_maximum_number_of_equal_rows/flip_columns_max_equal_rows.rb
@@ -0,0 +1,9 @@
+# @param {Integer[][]} matrix
+# @return {Integer}
+def max_equal_rows_after_flips(matrix)
+  require 'pry'; binding.pry
+end
+
+p max_equal_rows_after_flips([[0,1],[1,1]]) #1
+# p max_equal_rows_after_flips([[0,1],[1,0]]) #2
+# p max_equal_rows_after_flips([[0,0,0],[0,0,1],[1,1,0]]) #2
