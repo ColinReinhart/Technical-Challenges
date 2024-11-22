a63f64259eb8fd76580986f45744a36388095f28
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-22 11:13:37 -0700

    Passing all testcases

--- Leetcode/flip_columns_for_maximum_number_of_equal_rows/flip_columns_max_equal_rows.rb
+++ Leetcode/flip_columns_for_maximum_number_of_equal_rows/flip_columns_max_equal_rows.rb
@@ -1,34 +1,17 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
 def max_equal_rows_after_flips(matrix)
-  @max_matchy = 0
-  @wip = matrix
+  row_patterns = Hash.new(0)
 
-  def count_matchy_rows(mat)
-    count = 0
-    mat.each do |row|
-      if row.uniq.length == 1
-        count += 1
-      end
-    end
-    @max_matchy = count if count > @max_matchy
+  matrix.each do |row|
+    normalized = row.map { |val| val ^ row[0] }
+    row_patterns[normalized] += 1
   end
 
-  def reverse_binary_column(matrix, col_index)
-    matrix.each do |row|
-      row[col_index] = 1 - row[col_index]
-    end
-  end
-
-  matrix[0].length.times do |col_index|
-    reverse_binary_column(@wip, col_index)
-    count_matchy_rows(@wip)
-    reverse_binary_column(@wip, col_index)
-  end
-
-  @max_matchy
+  row_patterns.values.max
 end
 
-p max_equal_rows_after_flips([[0,1],[1,1]]) #1
-p max_equal_rows_after_flips([[0,1],[1,0]]) #2
-p max_equal_rows_after_flips([[0,0,0],[0,0,1],[1,1,0]]) #2
+p max_equal_rows_after_flips([[0, 1], [1, 1]]) # 1
+p max_equal_rows_after_flips([[0, 1], [1, 0]]) # 2
+p max_equal_rows_after_flips([[0, 0, 0], [0, 0, 1], [1, 1, 0]]) # 2
+p max_equal_rows_after_flips([[1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0], [1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1]]) # 2
