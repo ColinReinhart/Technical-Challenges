9a4496ab0c3580ffd69f4730337ce045cd545053
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-25 13:32:49 -0700

    Setup problem

---
+++ Leetcode/sliding_puzzle/sliding_puzzle.rb
@@ -0,0 +1,9 @@
+# @param {Integer[][]} board
+# @return {Integer}
+def sliding_puzzle(board)
+  require 'pry'; binding.pry
+end
+
+p sliding_puzzle([[1,2,3],[4,0,5]]) #1
+# p sliding_puzzle([[1,2,3],[5,4,0]]) #-1
+# p sliding_puzzle([[4,1,2],[5,0,3]]) #5
