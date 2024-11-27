e50a211a6736ab794117e24da3d90f3c1c975b9c
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-27 13:40:07 -0700

    Setup Problem

---
+++ Leetcode/shortest_distance_after_road_addition_queries_i/shortest_distance.rb
@@ -0,0 +1,9 @@
+# @param {Integer} n
+# @param {Integer[][]} queries
+# @return {Integer[]}
+def shortest_distance_after_queries(n, queries)
+  require 'pry'; binding.pry
+end
+
+p shortest_distance_after_queries(5, [[2,4],[0,2],[0,4]]) #[3,2,1]
+p shortest_distance_after_queries(4, [[0,3],[0,2]]) #[1,1]
