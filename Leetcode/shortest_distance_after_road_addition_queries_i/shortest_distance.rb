87eba05308a25a2d6a8d7bc4f6a79a01e5e174e2
Colin Reinhart <colinbreinhart@gmail.com>
2024-11-27 14:12:37 -0700

    Passing testcases 1-196

--- Leetcode/shortest_distance_after_road_addition_queries_i/shortest_distance.rb
+++ Leetcode/shortest_distance_after_road_addition_queries_i/shortest_distance.rb
@@ -2,7 +2,15 @@
 # @param {Integer[][]} queries
 # @return {Integer[]}
 def shortest_distance_after_queries(n, queries)
-  require 'pry'; binding.pry
+  answer = []
+  road = (0..n-1).to_a
+
+  queries.each do |x, y|
+    road.reject! { |city| city > x && city < y }
+    answer << road.length - 1
+  end
+
+  answer
 end
 
 p shortest_distance_after_queries(5, [[2,4],[0,2],[0,4]]) #[3,2,1]
