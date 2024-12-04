c051b84800d9e9a2f933157cecb7114b6308f7df
Colin Reinhart <colinbreinhart@gmail.com>
2024-12-04 10:02:55 -0700

    Setup problem

---
+++ Leetcode/check_if_n_and_its_double_exists/check_if_n_and_its_double_exist.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def check_if_exist(arr)
+  require 'pry'; binding.pry
+end
+
+p check_if_exist([10,2,5,3]) #true
+# p check_if_exist([3,1,7,11]) #false
