7ee0d80fa192440a3139e078f76062f8f95ec2db
Colin Reinhart <colinbreinhart@gmail.com>
2024-12-04 13:22:52 -0700

    Passing 1-105

--- Leetcode/check_if_n_and_its_double_exists/check_if_n_and_its_double_exist.rb
+++ Leetcode/check_if_n_and_its_double_exists/check_if_n_and_its_double_exist.rb
@@ -1,8 +1,15 @@
 # @param {Integer[]} arr
 # @return {Boolean}
 def check_if_exist(arr)
-  require 'pry'; binding.pry
+
+  arr.each do |num|
+    if num != 0 && arr.include?(num * 2)
+      return true
+    end
+  end
+
+  false
 end
 
 p check_if_exist([10,2,5,3]) #true
-# p check_if_exist([3,1,7,11]) #false
+p check_if_exist([3,1,7,11]) #false
