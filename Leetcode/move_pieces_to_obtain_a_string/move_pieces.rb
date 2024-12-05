2ebcacbd1cd7d99c59ae33e8a38e30473d73f898
Colin Reinhart <colinbreinhart@gmail.com>
2024-12-05 14:10:39 -0700

    Setup problem

---
+++ Leetcode/move_pieces_to_obtain_a_string/move_pieces.rb
@@ -0,0 +1,9 @@
+# @param {String} start
+# @param {String} target
+# @return {Boolean}
+def can_change(start, target)
+   require 'pry'; binding.pry
+end
+
+p can_change("_L__R__R_", "L______RR") #true
+# p can_change("R_L_", "__LR") #false
