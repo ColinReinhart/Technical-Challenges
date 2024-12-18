3f73d175e98ac5019dfa1d93c1301ede33d2285d
Colin Reinhart <colinbreinhart@gmail.com>
2024-12-18 11:40:11 -0700

    Setup problem

---
+++ Leetcode/final_prices_with_a_special_discount_in_a_shop/discount.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} prices
+# @return {Integer[]}
+def final_prices(prices)
+    require 'pry'; binding.pry
+end
+
+p final_prices([8,4,6,2,3]) #[4,2,4,2,3]
+# p final_prices([1,2,3,4,5]) #[1,2,3,4,5]
+# p final_prices([10,1,1,6]) #[9,0,1,6]
