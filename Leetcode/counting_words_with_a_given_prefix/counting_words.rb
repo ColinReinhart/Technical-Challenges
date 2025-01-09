d68a257577d947bebcda31fe8a8778f7d43d6a31
Colin Reinhart <colinbreinhart@gmail.com>
2025-01-09 13:58:27 -0700

    Passing all testcases

--- Leetcode/counting_words_with_a_given_prefix/counting_words.rb
+++ Leetcode/counting_words_with_a_given_prefix/counting_words.rb
@@ -2,8 +2,12 @@
 # @param {String} pref
 # @return {Integer}
 def prefix_count(words, pref)
-  require 'pry'; binding.pry
+  count = 0
+  size = pref.length - 1
+  words.each { |word| count += 1 if word[0..size] == pref }
+
+  count
 end
 
 p prefix_count(["pay","attention","practice","attend"], "at") #2
-# p prefix_count(["leetcode","win","loops","success"], "code") #0
+p prefix_count(["leetcode","win","loops","success"], "code") #0
