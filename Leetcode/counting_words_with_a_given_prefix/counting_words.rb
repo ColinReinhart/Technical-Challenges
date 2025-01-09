6e6843791e264e02be7c0622f08da19a3b2f504e
Colin Reinhart <colinbreinhart@gmail.com>
2025-01-09 13:53:33 -0700

    Setup problem

---
+++ Leetcode/counting_words_with_a_given_prefix/counting_words.rb
@@ -0,0 +1,9 @@
+# @param {String[]} words
+# @param {String} pref
+# @return {Integer}
+def prefix_count(words, pref)
+  require 'pry'; binding.pry
+end
+
+p prefix_count(["pay","attention","practice","attend"], "at") #2
+# p prefix_count(["leetcode","win","loops","success"], "code") #0
