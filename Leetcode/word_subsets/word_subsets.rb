1e267eaee023ef04544c0f48775a0961a14dc6be
Colin Reinhart <colinbreinhart@gmail.com>
2025-01-10 10:10:20 -0700

    Setup Problem

---
+++ Leetcode/word_subsets/word_subsets.rb
@@ -0,0 +1,9 @@
+# @param {String[]} words1
+# @param {String[]} words2
+# @return {String[]}
+def word_subsets(words1, words2)
+  require 'pry'; binding.pry
+end
+
+p word_subsets(["amazon","apple","facebook","google","leetcode"], ["e","o"]) #["facebook","google","leetcode"]
+# p word_subsets(["amazon","apple","facebook","google","leetcode"], ["l","e"]) #["apple","google","leetcode"]
