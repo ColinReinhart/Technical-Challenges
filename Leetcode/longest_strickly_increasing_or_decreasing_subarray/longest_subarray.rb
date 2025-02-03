6cdfd89ed654c8b2c40319d2e24c380049230cd1
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-03 11:54:14 -0700

    Passing all testcases

--- Leetcode/longest_strickly_increasing_or_decreasing_subarray/longest_subarray.rb
+++ Leetcode/longest_strickly_increasing_or_decreasing_subarray/longest_subarray.rb
@@ -1,11 +1,37 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def longest_monotonic_subarray(nums)
-  largest = 0
+  arrays = []
+  array = []
+  nums.each do |num|
+      if array == []
+        array << num
+      elsif num > array[-1]
+        array << num
+      else
+        arrays << array
+        array = [num]
+      end
+    end
+    arrays << array
 
-    require 'pry'; binding.pry
+    array = []
+
+    nums.each do |num|
+      if array == []
+        array << num
+      elsif num < array[-1]
+        array << num
+      else
+        arrays << array
+        array = [num]
+      end
+    end
+    arrays << array
+
+    arrays.map(&:length).max || 0
 end
 
 p longest_monotonic_subarray([1,4,3,3,2]) #2
-# p longest_monotonic_subarray([3,3,3,3]) #1
-# p longest_monotonic_subarray([3,2,1]) #3
+p longest_monotonic_subarray([3,3,3,3]) #1
+p longest_monotonic_subarray([3,2,1]) #3
