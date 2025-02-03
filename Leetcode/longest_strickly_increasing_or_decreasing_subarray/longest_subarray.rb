242de95e3a5a97475ae22bcc7695bc6c03ebda46
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-03 09:51:35 -0700

    Setup Problem

---
+++ Leetcode/longest_strickly_increasing_or_decreasing_subarray/longest_subarray.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def longest_monotonic_subarray(nums)
+  largest = 0
+
+    require 'pry'; binding.pry
+end
+
+p longest_monotonic_subarray([1,4,3,3,2]) #2
+# p longest_monotonic_subarray([3,3,3,3]) #1
+# p longest_monotonic_subarray([3,2,1]) #3
