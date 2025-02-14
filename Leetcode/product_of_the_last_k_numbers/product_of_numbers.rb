6012456530ae667f4ba6d40b49196cd19139d6d5
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-14 10:48:57 -0700

    Passing all testcases and Markdown written

--- Leetcode/product_of_the_last_k_numbers/product_of_numbers.rb
+++ Leetcode/product_of_the_last_k_numbers/product_of_numbers.rb
@@ -1,30 +1,34 @@
 class ProductOfNumbers
-    def initialize()
+  def initialize()
+    @prefix_products = [1]  # Prefix product list (1 is neutral for multiplication)
+  end
 
+  def add(num)
+    if num == 0
+      @prefix_products = [1]  # Reset the product list on zero
+    else
+      @prefix_products << @prefix_products[-1] * num
     end
+  end
 
+  def get_product(k)
+    return 0 if k >= @prefix_products.length  # If k exceeds stored values, zero was in the list
 
-=begin
-    :type num: Integer
-    :rtype: Void
-=end
-    def add(num)
-
-    end
-
-
-=begin
-    :type k: Integer
-    :rtype: Integer
-=end
-    def get_product(k)
-
-    end
-
-
+    @prefix_products[-1] / @prefix_products[-k-1]  # Get the product efficiently
+  end
 end
 
-# Your ProductOfNumbers object will be instantiated and called as such:
-# obj = ProductOfNumbers.new()
-# obj.add(num)
-# param_2 = obj.get_product(k)
+# ✅ Example Usage:
+productOfNumbers = ProductOfNumbers.new()
+productOfNumbers.add(3)   # [3]
+productOfNumbers.add(0)   # [3, 0] (resets prefix)
+productOfNumbers.add(2)   # [2]
+productOfNumbers.add(5)   # [2, 5]
+productOfNumbers.add(4)   # [2, 5, 4]
+
+puts productOfNumbers.get_product(2)  # 20 (5 * 4)
+puts productOfNumbers.get_product(3)  # 40 (2 * 5 * 4)
+puts productOfNumbers.get_product(4)  # 0  (Reset by previous 0)
+
+productOfNumbers.add(8)   # [2, 5, 4, 8]
+puts productOfNumbers.get_product(2)  # 32 (4 * 8)
