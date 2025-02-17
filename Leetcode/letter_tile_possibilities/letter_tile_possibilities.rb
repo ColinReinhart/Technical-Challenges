e332f6840cfcb0c36e764968d9393b93d748aa78
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-17 12:47:17 -0700

    Setup Problem

---
+++ Leetcode/letter_tile_possibilities/letter_tile_possibilities.rb
@@ -0,0 +1,9 @@
+# @param {String} tiles
+# @return {Integer}
+def num_tile_possibilities(tiles)
+  require 'pry'; binding.pry
+end
+
+p num_tile_possibilities("AAB") #8
+# p num_tile_possibilities("AAABBC") #188
+# p num_tile_possibilities("V") #1
