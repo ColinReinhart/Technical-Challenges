3b8d1410c0406fcd527afa5d394a63c51004080c
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-18 13:50:18 -0700

    Setup Problem

---
+++ Leetcode/construct_smallest_number_from_DI_string/smallest_number.rb
@@ -0,0 +1,8 @@
+# @param {String} pattern
+# @return {String}
+def smallest_number(pattern)
+    require 'pry'; binding.pry
+end
+
+p smallest_number("IIIDIDDD") #"123549876"
+# p smallest_number("DDD") #"4321"
