820ac765eac98ed7c3328fce0eee5ad81812801e
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-19 10:54:34 -0700

    Passing testcases 1-143, time limit exceeded

--- Leetcode/k-th_lexicographical_string_of_all_happy_strings/get_happy_string.rb
+++ Leetcode/k-th_lexicographical_string_of_all_happy_strings/get_happy_string.rb
@@ -2,9 +2,13 @@
 # @param {Integer} k
 # @return {String}
 def get_happy_string(n, k)
-  require 'pry'; binding.pry
+  set = ["a", "b", "c"]
+
+  wip = set.repeated_permutation(n).select { |arr| arr.each_cons(2).none? { |a, b| a == b } }.map(&:join).sort
+
+  wip[k-1] || ""
 end
 
 p get_happy_string(1, 3) #"c"
-# p get_happy_string(1, 4) #""
-# p get_happy_string(3, 9) #"cab"
+p get_happy_string(1, 4) #""
+p get_happy_string(3, 9) #"cab"
