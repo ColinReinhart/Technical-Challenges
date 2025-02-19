9615f525ea1a794ecb183c589495381f3aef38dc
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-19 10:12:37 -0700

    Setup Problem

---
+++ Leetcode/k-th_lexicographical_string_of_all_happy_strings/get_happy_string.rb
@@ -0,0 +1,10 @@
+# @param {Integer} n
+# @param {Integer} k
+# @return {String}
+def get_happy_string(n, k)
+  require 'pry'; binding.pry
+end
+
+p get_happy_string(1, 3) #"c"
+# p get_happy_string(1, 4) #""
+# p get_happy_string(3, 9) #"cab"
