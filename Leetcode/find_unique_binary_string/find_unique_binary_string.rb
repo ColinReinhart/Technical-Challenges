a0f6df553a3e3a4fdf1884ac11d96465b7679ce5
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-20 11:31:46 -0700

    Passing all testcases

--- Leetcode/find_unique_binary_string/find_unique_binary_string.rb
+++ Leetcode/find_unique_binary_string/find_unique_binary_string.rb
@@ -1,12 +1,9 @@
 # @param {String[]} nums
 # @return {String}
+# @param {String[]} nums
+# @return {String}
 def find_different_binary_string(nums)
-  n = nums[0].length
-  all = ["0", "1"].repeated_permutation(n).map(&:join)
-  hash = Hash.new(0)
-  nums.each { |num| hash[num] += 1 }
-  all.each { |num| hash[num] += 1 }
-  return hash.key(1)
+  nums.each_with_index.map { |num, i| num[i] == "0" ? "1" : "0" }.join
 end
 
 p find_different_binary_string(["01","10"]) # "11" or "00"
