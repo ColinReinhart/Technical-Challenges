167d9238a7e1b040dd17f57f9b849ae5f0d49bfd
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-24 11:26:08 -0700

    Setup problem

---
+++ Leetcode/most_profitable_path_in_a_tree/most_profitable_path.rb
@@ -0,0 +1,10 @@
+# @param {Integer[][]} edges
+# @param {Integer} bob
+# @param {Integer[]} amount
+# @return {Integer}
+def most_profitable_path(edges, bob, amount)
+  require 'pry'; binding.pry
+end
+
+p most_profitable_path([[0,1],[1,2],[1,3],[3,4]], 3, [-2,4,2,-4,6]) #6
+# p most_profitable_path([[0,1]], 1, [-7280,2350]) #-7280
