3c0d2c587568dc6dbf2563bd7d90397207b6f466
Colin Reinhart <colinbreinhart@gmail.com>
2025-02-25 10:08:14 -0700

    Passing testcases 1-85, Time limit exceeded

--- Leetcode/number_of_sub-arrays_with_odd_sum/README.md
+++ Leetcode/number_of_sub-arrays_with_odd_sum/README.md
@@ -8,8 +8,6 @@ Given an array of integers arr, return the number of subarrays with an odd sum.
 
 Since the answer can be very large, return it modulo 109 + 7.
 
- 
-
 Example 1:
 
 Input: arr = [1,3,5]
@@ -17,6 +15,7 @@ Output: 4
 Explanation: All subarrays are [[1],[1,3],[1,3,5],[3],[3,5],[5]]
 All sub-arrays sum are [1,4,9,3,8,5].
 Odd sums are [1,9,3,5] so the answer is 4.
+
 Example 2:
 
 Input: arr = [2,4,6]
--- Leetcode/number_of_sub-arrays_with_odd_sum/num_of_subarrays.rb
+++ Leetcode/number_of_sub-arrays_with_odd_sum/num_of_subarrays.rb
@@ -1,8 +1,24 @@
 # @param {Integer[]} arr
 # @return {Integer}
 def num_of_subarrays(arr)
-  require 'pry'; binding.pry
+  counter = 0
+  left = 0
+  right = 1
+
+  arr.each { |num| counter += 1 if num.odd? }
+
+  until left == arr.length - 1 do
+    counter += 1 if arr[left..right].sum.odd?
+    if right < arr.length - 1
+      right += 1
+    else
+      left += 1
+      right = left + 1
+    end
+  end
+
+  return counter
 end
 
-num_of_subarrays([1,3,5]) #4
-# num_of_subarrays([2,4,6]) #0
+p num_of_subarrays([1,3,5]) #4
+p num_of_subarrays([2,4,6]) #0
