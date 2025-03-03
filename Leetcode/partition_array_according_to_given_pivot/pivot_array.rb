98fb22a9c783508d45506dccd199584998de3769
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-03 09:41:36 -0700

    Setup problem

---
+++ Leetcode/partition_array_according_to_given_pivot/pivot_array.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @param {Integer} pivot
+# @return {Integer[]}
+def pivot_array(nums, pivot)
+  require 'pry'; binding.pry
+end
+
+p pivot_array([9, 12, 3, 5, 14, 10, 10], 10)
+# => [9, 3, 5, 10, 10, 12, 14]
+# p pivot_array([-3,4,3,2], 2)
+# => [-3, 2, 4, 3]
