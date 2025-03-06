a559bdf3b11db3a1f510beea241e8016f22fa97a
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-06 09:52:05 -0700

    Passing all testcases

--- Leetcode/find_missing_and_repeated_values/find_missing_and_repeated_values.rb
+++ Leetcode/find_missing_and_repeated_values/find_missing_and_repeated_values.rb
@@ -1,8 +1,15 @@
 # @param {Integer[][]} grid
 # @return {Integer[]}
 def find_missing_and_repeated_values(grid)
-  require 'pry'; binding.pry
+  answer = []
+  n = grid.flatten.length
+  hash = (1..n).map { |key| [key, 0] }.to_h
+  grid.flatten.each { |num| hash[num] += 1 }
+  answer << hash.key(2)
+  answer << hash.key(0)
+
+  return answer
 end
 
 p find_missing_and_repeated_values([[1,3],[2,2]]) #[2,4]
-# p find_missing_and_repeated_values([[9,1,7],[8,9,2],[3,4,6]]) #[9,5]
+p find_missing_and_repeated_values([[9,1,7],[8,9,2],[3,4,6]]) #[9,5]
