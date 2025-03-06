2493cf3e0fb9920f3ecc3bb6c9bd97ccb311da6b
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-06 09:25:32 -0700

    Setup problem

---
+++ Leetcode/find_missing_and_repeated_values/find_missing_and_repeated_values.rb
@@ -0,0 +1,8 @@
+# @param {Integer[][]} grid
+# @return {Integer[]}
+def find_missing_and_repeated_values(grid)
+  require 'pry'; binding.pry
+end
+
+p find_missing_and_repeated_values([[1,3],[2,2]]) #[2,4]
+# p find_missing_and_repeated_values([[9,1,7],[8,9,2],[3,4,6]]) #[9,5]
