982d7b9ea30a7133e7ab782d8b93c279110a9ca7
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-07 16:06:30 -0700

    Setup Problem

---
+++ Leetcode/closest_prime_numbers_in_range/closest_prime.rb
@@ -0,0 +1,9 @@
+# @param {Integer} left
+# @param {Integer} right
+# @return {Integer[]}
+def closest_primes(left, right)
+  require 'pry'; binding.pry
+end
+
+p closest_primes(10, 19) #[11,13]
+# p closest_primes(4, 6) #[-1,-1]
