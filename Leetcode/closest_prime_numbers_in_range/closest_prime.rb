10b2a6ba282981b6893f1aa06c2b616ecd518055
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-07 16:09:31 -0700

    Passing testcases 1-56, time limit exceeded on 57

--- Leetcode/closest_prime_numbers_in_range/closest_prime.rb
+++ Leetcode/closest_prime_numbers_in_range/closest_prime.rb
@@ -2,8 +2,31 @@
 # @param {Integer} right
 # @return {Integer[]}
 def closest_primes(left, right)
-  require 'pry'; binding.pry
+  sieve = Array.new(right + 1, true)
+  sieve[0] = sieve[1] = false
+
+  (2..Math.sqrt(right)).each do |i|
+    next unless sieve[i]
+    (i * i..right).step(i) { |j| sieve[j] = false }
+  end
+
+  primes = (left..right).select { |num| sieve[num] }
+
+  return [-1, -1] if primes.size < 2
+
+  min_gap = Float::INFINITY
+  closest_pair = [-1, -1]
+
+  (0...primes.size - 1).each do |i|
+    diff = primes[i + 1] - primes[i]
+    if diff < min_gap
+      min_gap = diff
+      closest_pair = [primes[i], primes[i + 1]]
+    end
+  end
+
+  closest_pair
 end
 
 p closest_primes(10, 19) #[11,13]
-# p closest_primes(4, 6) #[-1,-1]
+p closest_primes(4, 6) #[-1,-1]
