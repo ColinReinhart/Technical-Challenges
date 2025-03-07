e305ee3b6d2f0de0a2b2d8112f724ef86fefc944
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-07 16:12:14 -0700

    Passing all testcases

--- Leetcode/closest_prime_numbers_in_range/closest_prime.rb
+++ Leetcode/closest_prime_numbers_in_range/closest_prime.rb
@@ -2,26 +2,39 @@
 # @param {Integer} right
 # @return {Integer[]}
 def closest_primes(left, right)
-  sieve = Array.new(right + 1, true)
-  sieve[0] = sieve[1] = false
+  return [-1, -1] if right < 2
 
-  (2..Math.sqrt(right)).each do |i|
-    next unless sieve[i]
-    (i * i..right).step(i) { |j| sieve[j] = false }
+  limit = Math.sqrt(right).to_i
+  is_prime = Array.new(limit + 1, true)
+  is_prime[0] = is_prime[1] = false
+
+  (2..limit).each do |i|
+    next unless is_prime[i]
+    (i * i..limit).step(i) { |j| is_prime[j] = false }
+  end
+  small_primes = (2..limit).select { |x| is_prime[x] }
+
+  range_size = right - left + 1
+  range_prime = Array.new(range_size, true)
+
+  small_primes.each do |p|
+    start = [p * p, (left + p - 1) / p * p].max
+    (start..right).step(p) { |j| range_prime[j - left] = false }
   end
 
-  primes = (left..right).select { |num| sieve[num] }
+  primes = (left..right).select { |i| i >= 2 && range_prime[i - left] }
 
   return [-1, -1] if primes.size < 2
 
   min_gap = Float::INFINITY
   closest_pair = [-1, -1]
 
-  (0...primes.size - 1).each do |i|
-    diff = primes[i + 1] - primes[i]
+  primes.each_cons(2) do |a, b|
+    diff = b - a
     if diff < min_gap
       min_gap = diff
-      closest_pair = [primes[i], primes[i + 1]]
+      closest_pair = [a, b]
+      break if diff == 2
     end
   end
 
