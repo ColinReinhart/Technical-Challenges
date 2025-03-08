2c9647a11fedb5ad7d4e355bb92f3aa6ca59074f
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-08 13:48:07 -0700

    Setup  Problem

---
+++ Leetcode/minimum_recolors_to_get_k_consecutive_black_blocks/minimum_recolors.rb
@@ -0,0 +1,9 @@
+# @param {String} blocks
+# @param {Integer} k
+# @return {Integer}
+def minimum_recolors(blocks, k)
+  require 'pry'; binding.pry
+end
+
+p minimum_recolors("WBBWWBBWBW", 7) #3
+# p minimum_recolors("WBWBBBW", 2) #0
