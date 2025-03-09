4019a823a8b347ad27019978c8003e5bd23f1c52
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-09 13:10:31 -0600

    Passing testcases 1-530, time limit exceeded

--- Leetcode/alternating_groups_II/alternating_groups_ii.rb
+++ Leetcode/alternating_groups_II/alternating_groups_ii.rb
@@ -2,7 +2,25 @@
 # @param {Integer} k
 # @return {Integer}
 def number_of_alternating_groups(colors, k)
-  require 'pry'; binding.pry
+  n = colors.length
+  extended_colors = colors + colors[0...(k - 1)]
+  count = 0
+
+  (0...(n)).each do |i|
+    group = extended_colors[i...(i + k)]
+    is_alternating = true
+
+    (1...(k - 1)).each do |j|
+      if group[j] == group[j - 1] || group[j] == group[j + 1]
+        is_alternating = false
+        break
+      end
+    end
+
+    count += 1 if is_alternating
+  end
+
+  count
 end
 
 p number_of_alternating_groups([0,1,0,1,0], 3) #3
