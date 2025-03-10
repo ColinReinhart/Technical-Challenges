df66dcfe6f72df736eeff4053ee1a65d74c998bd
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-10 12:47:25 -0600

    Setup problem

---
+++ Leetcode/count_of_substrings_containing_every_vowel_and_k_consonants_II/count_of_substrings.rb
@@ -0,0 +1,10 @@
+# @param {String} word
+# @param {Integer} k
+# @return {Integer}
+def count_of_substrings(word, k)
+  require 'pry'; binding.pry
+end
+
+p count_of_substrings("aeioqq", 1) #0
+# p count_of_substrings("aeiou", 0) #1
+# p count_of_substrings("ieaouqqieaouqq", 1) #3
