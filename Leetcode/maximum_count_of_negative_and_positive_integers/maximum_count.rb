6dbe0b02ac030b04f3bef65e9f9853f4f3de4a78
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-12 10:16:12 -0600

    Setup problem

---
+++ Leetcode/maximum_count_of_negative_and_positive_integers/maximum_count.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def maximum_count(nums)
+  require 'pry'; binding.pry
+end
+
+p maximum_count([-2,-1,-1,1,2,3]) #3
+# p maximum_count([-3,-2,-1,0,0,1,2]) #3
+# p maximum_count([5,20,66,1314]) #4
