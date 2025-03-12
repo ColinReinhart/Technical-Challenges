02fcabcc5d9a6dd4f26d1692258c1758d459f7ae
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-12 10:19:04 -0600

    Passing all testcases

--- Leetcode/maximum_count_of_negative_and_positive_integers/maximum_count.rb
+++ Leetcode/maximum_count_of_negative_and_positive_integers/maximum_count.rb
@@ -1,9 +1,20 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def maximum_count(nums)
-  require 'pry'; binding.pry
+  neg = 0
+  pos = 0
+
+  nums.each do |num|
+    if num > 0
+      pos += 1
+    elsif num < 0
+      neg += 1
+    end
+  end
+
+  return [neg, pos].max
 end
 
 p maximum_count([-2,-1,-1,1,2,3]) #3
-# p maximum_count([-3,-2,-1,0,0,1,2]) #3
-# p maximum_count([5,20,66,1314]) #4
+p maximum_count([-3,-2,-1,0,0,1,2]) #3
+p maximum_count([5,20,66,1314]) #4
