3c77afce977125793e441717b69ef14eb469a7ee
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-13 15:01:57 -0600

    PAssing testcases 1 - 620, time limit exceeded

--- Leetcode/zero_array_transformation_II/min_zero_array.rb
+++ Leetcode/zero_array_transformation_II/min_zero_array.rb
@@ -2,8 +2,28 @@
 # @param {Integer[][]} queries
 # @return {Integer}
 def min_zero_array(nums, queries)
-  require 'pry'; binding.pry
+  k = 0
+
+  def is_zero_array(arr)
+    arr.each { |num| return false if num != 0 }
+    return true
+  end
+
+  return 0 if is_zero_array(nums)
+
+  queries.each do |l, r, val|
+    (l..r).each do |i|
+      nums[i] = [nums[i] - val, 0].max
+    end
+
+    k += 1
+
+    return k if is_zero_array(nums)
+  end
+
+  -1
 end
 
 p min_zero_array([2,0,2], [[0,2,1],[0,2,1],[1,1,3]]) #2
-p min_zero_array([4,3,2,1],  [[1,3,2],[0,2,1]]) #2
+p min_zero_array([4,3,2,1],  [[1,3,2],[0,2,1]]) #-1
+p min_zero_array([0],  [[0,0,2],[0,0,4],[0,0,4],[0,0,3],[0,0,5]]) #0
