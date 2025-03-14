7e64887efbc65d506ee5149f2010c3a4df51cf11
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-14 12:03:39 -0600

    Setup problem

---
+++ Leetcode/maximum_candies_allocated_to_k_children/maximum_candies.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} candies
+# @param {Integer} k
+# @return {Integer}
+def maximum_candies(candies, k)
+  require 'pry'; binding.pry
+end
+
+p maximum_candies([5,8,6], 3) #5
+# p maximum_candies([2,5], 11) #0
