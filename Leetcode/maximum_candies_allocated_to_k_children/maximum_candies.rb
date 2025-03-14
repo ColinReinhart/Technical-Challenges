3c13b4a4fef901e96d1140d4b1b5fef0e7182f08
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-14 12:07:57 -0600

    Passing all testcases

--- Leetcode/maximum_candies_allocated_to_k_children/maximum_candies.rb
+++ Leetcode/maximum_candies_allocated_to_k_children/maximum_candies.rb
@@ -2,8 +2,25 @@
 # @param {Integer} k
 # @return {Integer}
 def maximum_candies(candies, k)
-  require 'pry'; binding.pry
+  left = 1
+  right = candies.max
+  result = 0
+
+  while left <= right
+    mid = (left + right) / 2
+
+    total = candies.sum { |candy| candy / mid }
+
+    if total >= k
+      result = mid
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+
+  result
 end
 
 p maximum_candies([5,8,6], 3) #5
-# p maximum_candies([2,5], 11) #0
+p maximum_candies([2,5], 11) #0
