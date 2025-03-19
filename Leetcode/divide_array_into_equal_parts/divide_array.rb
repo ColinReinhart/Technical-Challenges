1df472316b8ba3609b641b5d413575a2746c7daa
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-19 14:19:41 -0600

    Passing all testcases

--- Leetcode/divide_array_into_equal_parts/divide_array.rb
+++ Leetcode/divide_array_into_equal_parts/divide_array.rb
@@ -1,8 +1,18 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def divide_array(nums)
-  require 'pry'; binding.pry
+  answer = true
+  hash = Hash.new(0)
+  nums.each { |num| hash[num] += 1 }
+
+  hash.values.each do |num|
+    if num.odd?
+      answer = false
+    end
+  end
+
+  answer
 end
 
 p divide_array([3,2,3,2,2,2]) #true
-# p divide_array([1,2,3,4]) #false
+p divide_array([1,2,3,4]) #false
