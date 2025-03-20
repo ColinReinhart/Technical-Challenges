8160b9fe69504f4ddf674d682d772e41301d98fa
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-20 13:57:25 -0600

    Setup Problem

---
+++ Leetcode/minimum_cost_walk_in_weighted_graph/minimum_cost.rb
@@ -0,0 +1,10 @@
+# @param {Integer} n
+# @param {Integer[][]} edges
+# @param {Integer[][]} query
+# @return {Integer[]}
+def minimum_cost(n, edges, query)
+  require 'pry'; binding.pry
+end
+
+p  minimum_cost(5, [[0,1,7],[1,3,7],[1,2,1]], [[0,3],[3,4]]) #[1,-1]
+# p  minimum_cost(3, [[0,2,7],[0,1,15],[1,2,6],[1,2,1]], [[1,2]]) #[0]
