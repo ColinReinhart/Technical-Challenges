22582b79bc899b62debd9abf509a642a2c1cdd7c
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-24 09:40:39 -0600

    Passing testcases 1 - 554, time limit exceeded

--- Leetcode/count_days_without_meetings/count_days.rb
+++ Leetcode/count_days_without_meetings/count_days.rb
@@ -2,7 +2,14 @@
 # @param {Integer[][]} meetings
 # @return {Integer}
 def count_days(days, meetings)
-  require 'pry'; binding.pry
+  hash = Hash.new(0)
+  (1..days).to_a.each { |day| hash[day] += 1 }
+
+  meetings.each do |meeting|
+    (meeting[0]..meeting[1]).to_a.each { |day| hash[day] += 1 }
+  end
+
+  return hash.count { |_, value| value == 1 }
 end
 
 p count_days(10, [[5,7],[1,3],[9,10]]) #2
