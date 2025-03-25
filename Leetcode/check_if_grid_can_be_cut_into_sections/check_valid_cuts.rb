a240db7a4d5bc70464313b875580ee905a03c03d
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-25 13:39:01 -0600

    Passing all testcases

--- Leetcode/check_if_grid_can_be_cut_into_sections/check_valid_cuts.rb
+++ Leetcode/check_if_grid_can_be_cut_into_sections/check_valid_cuts.rb
@@ -2,51 +2,16 @@
 # @param {Integer[][]} rectangles
 # @return {Boolean}
 def check_valid_cuts(n, rectangles)
-  x_intervals = rectangles.map { |x1, _, x2, _| [x1, x2] }
-  y_intervals = rectangles.map { |_, y1, _, y2| [y1, y2] }
-
-  x_starts = x_intervals.map(&:first).uniq.sort
-  x_ends   = x_intervals.map(&:last).uniq.sort
-  y_starts = y_intervals.map(&:first).uniq.sort
-  y_ends   = y_intervals.map(&:last).uniq.sort
-
-  # Check vertical cuts
-  (0...x_ends.length).each do |i|
-    cut1 = x_ends[i]
-    (i + 1...x_starts.length).each do |j|
-      cut2 = x_starts[j]
-
-      # Define 3 vertical zones
-      left = rectangles.select { |x1, _, x2, _| x2 <= cut1 }
-      middle = rectangles.select { |x1, _, x2, _| x1 >= cut1 && x2 <= cut2 }
-      right = rectangles.select { |x1, _, x2, _| x1 >= cut2 }
-
-      all_non_empty = [left, middle, right].all?(&:any?)
-      no_overlap = (left + middle + right).uniq.length == rectangles.length
-
-      return true if all_non_empty && no_overlap
-    end
+  can = -> arr do
+    arr.sort! { |a, b| a[0] == b[0] ? a[1] <=> b[1] : a[0] <=> b[0] }
+    .inject([0, arr[0][1]]) { |(cnt, curr), с|
+      (return true if (cnt += 1) == 2) if с[0] >= curr
+      [cnt, [с[1], curr].max] } and false
   end
 
-  # Check horizontal cuts
-  (0...y_ends.length).each do |i|
-    cut1 = y_ends[i]
-    (i + 1...y_starts.length).each do |j|
-      cut2 = y_starts[j]
-
-      # Define 3 horizontal zones
-      bottom = rectangles.select { |_, y1, _, y2| y2 <= cut1 }
-      middle = rectangles.select { |_, y1, _, y2| y1 >= cut1 && y2 <= cut2 }
-      top = rectangles.select { |_, y1, _, y2| y1 >= cut2 }
-
-      all_non_empty = [bottom, middle, top].all?(&:any?)
-      no_overlap = (bottom + middle + top).uniq.length == rectangles.length
-
-      return true if all_non_empty && no_overlap
-    end
-  end
-
-  false
+    rectangles.each_with_object([[], []]) { |r, o|
+      o[0].push([r[0], r[2]]) and o[1].push([r[1], r[3]]) }
+    .yield_self{ |a1, a2| can[a1] || can[a2] }
 end
 
 p check_valid_cuts(5, [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]) #true
