9b261d20c298d53fb787438a1bf243dd7d57447f
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-26 10:09:51 -0600

    Setup Problem

---
+++ Leetcode/minimum_operations_to_make_a_uni-value_grid/min_operations.rb
@@ -0,0 +1,10 @@
+# @param {Integer[][]} grid
+# @param {Integer} x
+# @return {Integer}
+def min_operations(grid, x)
+  require 'pry'; binding.pry
+end
+
+p min_operations([[2,4],[6,8]], 2) #4
+# p min_operations([[1,5],[2,3]], 1) #5
+# p min_operations([[1,2],[3,4]], 2) #-1
