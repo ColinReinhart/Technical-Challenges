4ed10ac8b37d94386fe315c025137010df891c70
Colin Reinhart <colinbreinhart@gmail.com>
2025-03-27 11:10:23 -0600

    Setup Problem

---
+++ Leetcode/minimum_index_of_a_valid_split/minimum_index.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def minimum_index(nums)
+  require 'pry'; binding.pry
+end
+
+p minimum_index([1,2,2,2]) #2
+# p minimum_index([2,1,3,1,1,1,7,1,2,1]) #4
+# p minimum_index([3,3,3,3,7,2,2]) #-1
