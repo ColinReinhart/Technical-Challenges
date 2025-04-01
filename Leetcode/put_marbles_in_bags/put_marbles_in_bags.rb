4deb40c9a006e3d1e5d14e72c14fd75031354fe4
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-01 11:02:15 -0600

    Passing all testcases

--- Leetcode/put_marbles_in_bags/put_marbles_in_bags.rb
+++ Leetcode/put_marbles_in_bags/put_marbles_in_bags.rb
@@ -1,38 +1,16 @@
 # @param {Integer[]} weights
 # @param {Integer} k
 # @return {Integer}
-def split_into_k_contiguous_groups(weights, k, prefix = [], result = [])
-  return result << (prefix + [weights]) if k == 1 && !weights.empty?
-
-  (1..weights.size - k + 1).each do |i|
-    group = weights[0...i]
-    rest = weights[i..-1]
-    split_into_k_contiguous_groups(rest, k - 1, prefix + [group], result)
-  end
-
-  result
-end
-
 def put_marbles(weights, k)
-  max_score = 0
-  min_score = Float::INFINITY
-  scores = []
-
-  arrays = split_into_k_contiguous_groups(weights, k)
-
-  arrays.each do |arr|
-    score = 0
-    arr.each do |a|
-      score += a[0] + a[-1]
-    end
-    scores << score
-    if score > max_score
-      max_score = score
-    end
-    if score < min_score
-      min_score = score
-    end
+  pair_sums = []
+  (0...weights.size - 1).each do |i|
+    pair_sums << weights[i] + weights[i + 1]
   end
+
+  sorted = pair_sums.sort
+
+  max_score = sorted.last(k - 1).sum
+  min_score = sorted.first(k - 1).sum
 
   max_score - min_score
 end
