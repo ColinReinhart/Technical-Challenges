d5ff0867d14d3b5602e76733ce6be242e84525e8
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-01 11:14:47 -0600

    Setup problem

---
+++ Leetcode/solving_problems_with_brainpower/most_points.rb
@@ -0,0 +1,8 @@
+# @param {Integer[][]} questions
+# @return {Integer}
+def most_points(questions)
+  require 'pry'; binding.pry
+end
+
+p most_points([[3,2],[4,3],[4,4],[2,5]]) #5
+# p most_points([[1,1],[2,2],[3,3],[4,4],[5,5]]) #7
