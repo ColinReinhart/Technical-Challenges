e73f792b0f5da016f76e715d8fd65eda13ecd464
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-03 14:42:39 -0600

    Setup Problem

---
+++ Leetcode/maximum_value_of_an_ordered_triplet_II/maximum_triplet_value.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def maximum_triplet_value(nums)
+    require 'pry'; binding.pry
+end
+
+p maximum_triplet_value([12,6,1,2,7]) #77
+# p maximum_triplet_value([1,10,3,4,19]) #133
+# p maximum_triplet_value([1,2,3]) #0
