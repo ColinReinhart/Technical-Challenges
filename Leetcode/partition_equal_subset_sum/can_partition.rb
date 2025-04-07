0a69ea3ba2b637a3f0a79b39e75ab3ce83d7b06c
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-07 09:53:26 -0600

    Passing [1,1] but not it is taking too long, time limit exceeded on TC 37

--- Leetcode/partition_equal_subset_sum/can_partition.rb
+++ Leetcode/partition_equal_subset_sum/can_partition.rb
@@ -1,13 +1,21 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_partition(nums)
+  n = nums.size
 
-  (0..nums.size).each do |n|
-    nums.combination(n).each do |first|
-      second = nums - first
-      pair = [first, second]
-      return true if first.sum == second.sum
+  (0...(1 << n)).each do |mask|
+    left = []
+    right = []
+
+    nums.each_with_index do |val, i|
+      if mask[i] == 1
+        left << val
+      else
+        right << val
+      end
     end
+
+    return true if left.sum == right.sum
   end
 
   false
@@ -16,3 +24,4 @@ end
 p can_partition([1,5,11,5]) #true
 p can_partition([1,2,3,5]) #false
 p can_partition([2,2,1,1]) #true
+p can_partition([1,1]) #true
