038720ae35a63effeed389aad5e619e66a09d0a4
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-07 09:19:09 -0600

    Passing testcases 1-72

--- Leetcode/partition_equal_subset_sum/can_partition.rb
+++ Leetcode/partition_equal_subset_sum/can_partition.rb
@@ -1,8 +1,20 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_partition(nums)
-  require 'pry'; binding.pry
+  nums.sort!
+  point = 1
+
+  until nums[0..point].sum > nums[point+1..-1].sum || nums[point] == nil
+    if nums[0..point].sum == nums[point+1..-1].sum
+      return true
+    else
+      point += 1
+    end
+  end
+
+  false
 end
 
-p can_partition([1,5,11,5]) #true
+# p can_partition([1,5,11,5]) #true
 # p can_partition([1,2,3,5]) #false
+p can_partition([2,2,1,1]) #true
