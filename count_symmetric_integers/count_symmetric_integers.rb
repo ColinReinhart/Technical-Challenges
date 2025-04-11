956e86c29962e8aaa6552d63859f014727fde247
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-11 09:32:56 -0600

    Setup Problem

---
+++ count_symmetric_integers/count_symmetric_integers.rb
@@ -0,0 +1,9 @@
+# @param {Integer} low
+# @param {Integer} high
+# @return {Integer}
+def count_symmetric_integers(low, high)
+  require 'pry'; binding.pry
+end
+
+p count_symmetric_integers(1, 100) #9
+# p count_symmetric_integers(1200, 1230) #4
