6727356a837fd370040145ef0c03a7f8ab229580
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-14 10:20:15 -0600

    Passing all testcases

--- Leetcode/count_good_triplets/count_good_triplets.rb
+++ Leetcode/count_good_triplets/count_good_triplets.rb
@@ -4,8 +4,22 @@
 # @param {Integer} c
 # @return {Integer}
 def count_good_triplets(arr, a, b, c)
-  require 'pry'; binding.pry
+  count = 0
+
+  (0...arr.length).each do |i|
+    (i+1...arr.length).each do |j|
+      next unless (arr[i] - arr[j]).abs <= a
+
+      (j+1...arr.length).each do |k|
+        if (arr[j] - arr[k]).abs <= b && (arr[i] - arr[k]).abs <= c
+          count += 1
+        end
+      end
+    end
+  end
+
+  count
 end
 
 p count_good_triplets([3,0,1,1,9,7], 7, 2, 3) #4
-# p count_good_triplets([1,1,2,2,3], 0, 0, 1) #0
+p count_good_triplets([1,1,2,2,3], 0, 0, 1) #0
