1379274613c942eaf41bc6fc6ec35af24b4a7f4d
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-14 09:30:32 -0600

    Setup Problem

---
+++ Leetcode/count_good_triplets/count_good_triplets.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} arr
+# @param {Integer} a
+# @param {Integer} b
+# @param {Integer} c
+# @return {Integer}
+def count_good_triplets(arr, a, b, c)
+  require 'pry'; binding.pry
+end
+
+p count_good_triplets([3,0,1,1,9,7], 7, 2, 3) #4
+# p count_good_triplets([1,1,2,2,3], 0, 0, 1) #0
