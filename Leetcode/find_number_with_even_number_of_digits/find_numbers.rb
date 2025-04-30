ddcac36728195d5890148b3a681f973f16188d33
Colin Reinhart <colinbreinhart@gmail.com>
2025-04-30 10:11:25 -0600

    Setup Problem

---
+++ Leetcode/find_number_with_even_number_of_digits/find_numbers.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_numbers(nums)
+  require 'pry'; binding.pry
+end
+
+p find_numbers([12,345,2,6,7896]) #2
+# p find_numbers([555,901,482,1771]) #1
