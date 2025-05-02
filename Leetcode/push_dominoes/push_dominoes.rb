cca25d4ff72f53b1929889785f1e81cc7f71f1bc
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-02 12:44:52 -0600

    Setup Problem

---
+++ Leetcode/push_dominoes/push_dominoes.rb
@@ -0,0 +1,8 @@
+# @param {String} dominoes
+# @return {String}
+def push_dominoes(dominoes)
+  require 'pry'; binding.pry
+end
+
+p push_dominoes("RR.L") #"RR.L"
+# p push_dominoes(".L.R...LR..L..") #"LL.RR.LLRRLL.."
