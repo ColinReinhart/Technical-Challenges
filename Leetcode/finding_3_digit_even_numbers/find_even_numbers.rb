56de897ced5a8aa056355ae9e5558303717cfc39
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-12 14:24:54 -0600

    Passing all testcases

--- Leetcode/finding_3_digit_even_numbers/find_even_numbers.rb
+++ Leetcode/finding_3_digit_even_numbers/find_even_numbers.rb
@@ -1,16 +1,32 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def find_even_numbers(digits)
-  even = []
-  wip = digits.permutation(3).to_a.map { |arr| arr.join.to_i }
+  freq = Array.new(10, 0)
+  digits.each { |d| freq[d] += 1 }
 
-  wip.each do |num|
-    even << num if num.to_s.length == 3 && num.even?
+  result = []
+
+  (100..998).step(2) do |num|
+    a, b, c = num / 100, (num / 10) % 10, num % 10
+    used = Array.new(10, 0)
+    used[a] += 1
+    used[b] += 1
+    used[c] += 1
+
+    valid = true
+    (0..9).each do |d|
+      if used[d] > freq[d]
+        valid = false
+        break
+      end
+    end
+
+    result << num if valid
   end
 
-  even.uniq.sort
+  result
 end
 
-# p find_even_numbers([2,1,3,0]) == [102,120,130,132,210,230,302,310,312,320]
+p find_even_numbers([2,1,3,0]) == [102,120,130,132,210,230,302,310,312,320]
 p find_even_numbers([2,2,8,8,2]) == [222,228,282,288,822,828,882]
-# p find_even_numbers([3,7,5]) == []
+p find_even_numbers([3,7,5]) == []
