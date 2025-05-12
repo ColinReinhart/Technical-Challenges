abe27f0c4ffa18e69c46ec92129060b560babe5c
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-12 13:43:36 -0600

    Srtup Problem

---
+++ Leetcode/finding_3_digit_even_numbers/find_even_numbers.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} digits
+# @return {Integer[]}
+def find_even_numbers(digits)
+  require 'pry'; binding.pry
+end
+
+p find_even_numbers([2,1,3,0]) #[102,120,130,132,210,230,302,310,312,320]
+# p find_even_numbers([2,2,8,8,2]) #[222,228,282,288,822,828,882]
+# p find_even_numbers([3,7,5]) #[]
