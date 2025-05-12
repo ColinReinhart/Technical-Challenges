43d44d784d652420ee6871c0d1614c9aee4bb9d8
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-12 14:11:44 -0600

    Passing base testcases

--- Leetcode/finding_3_digit_even_numbers/find_even_numbers.rb
+++ Leetcode/finding_3_digit_even_numbers/find_even_numbers.rb
@@ -1,9 +1,16 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def find_even_numbers(digits)
-  require 'pry'; binding.pry
+  even = []
+  wip = digits.permutation(3).to_a.map { |arr| arr.join.to_i }
+
+  wip.each do |num|
+    even << num if num.to_s.length == 3 && num.even?
+  end
+
+  even.uniq.sort
 end
 
-p find_even_numbers([2,1,3,0]) #[102,120,130,132,210,230,302,310,312,320]
-# p find_even_numbers([2,2,8,8,2]) #[222,228,282,288,822,828,882]
-# p find_even_numbers([3,7,5]) #[]
+# p find_even_numbers([2,1,3,0]) == [102,120,130,132,210,230,302,310,312,320]
+p find_even_numbers([2,2,8,8,2]) == [222,228,282,288,822,828,882]
+# p find_even_numbers([3,7,5]) == []
