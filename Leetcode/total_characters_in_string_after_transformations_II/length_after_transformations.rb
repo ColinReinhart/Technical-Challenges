5ccb0ea12a6558c470fff7f5a5b20d8c5a835b1d
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-14 14:13:12 -0600

    Setup Problem

---
+++ Leetcode/total_characters_in_string_after_transformations_II/length_after_transformations.rb
@@ -0,0 +1,10 @@
+# @param {String} s
+# @param {Integer} t
+# @param {Integer[]} nums
+# @return {Integer}
+def length_after_transformations(s, t, nums)
+  require 'pry'; binding.pry
+end
+
+p length_after_transformations("abcyy", 2, [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2]) #7 "cdeabab"
+# p length_after_transformations("azbk", 1, [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]) #8 "bcabcdlm"
