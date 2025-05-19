bedf053589b6494fed76276b39f2d758049f144d
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-19 11:50:29 -0600

    Setup Problem

---
+++ Leetcode/type_of_triangle/type_of_triangle.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {String}
+def triangle_type(nums)
+  require 'pry'; binding.pry
+end
+
+p triangle_type([3,3,3]) #"equilateral"
+# p triangle_type([3,4,5]) #"scalene"
