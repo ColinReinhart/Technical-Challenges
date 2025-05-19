20d0ec6841e5d84d57a7167fbdd8a296dc2d6daa
Colin Reinhart <colinbreinhart@gmail.com>
2025-05-19 12:06:50 -0600

    Passing all testcases

--- Leetcode/type_of_triangle/type_of_triangle.rb
+++ Leetcode/type_of_triangle/type_of_triangle.rb
@@ -1,8 +1,24 @@
 # @param {Integer[]} nums
 # @return {String}
 def triangle_type(nums)
-  require 'pry'; binding.pry
+  nums.sort!
+  return "none" if nums[0] + nums[1] <= nums[2]
+
+  sides = Hash.new(0)
+  nums.each { |side| sides[side] += 1 }
+
+  if sides.count == 1
+    return "equilateral"
+  elsif sides.count == 2
+    return "isosceles"
+  elsif sides.count == 3
+    return "scalene"
+  else
+    return "none"
+  end
+
 end
 
 p triangle_type([3,3,3]) #"equilateral"
-# p triangle_type([3,4,5]) #"scalene"
+p triangle_type([3,4,5]) #"scalene"
+p triangle_type([8,4,2]) #"none"
