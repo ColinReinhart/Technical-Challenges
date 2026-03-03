9b3d2ef72b30a64e2af09be113225c837900371e
Colin Reinhart (he/him) <ColinBReinhart@gmail.com>
2026-03-03 10:59:05 -0700

    Passing testcases 1-52, time limit exceeding on test 53

---
+++ Leetcode/find_kth_bit_in_nth_binary_string/find_kth_bit.rb
@@ -0,0 +1,31 @@
+# @param {Integer} n
+# @param {Integer} k
+# @return {Character}
+def find_kth_bit(n, k)
+    sn = "0"
+
+    if n > 1 
+        (n - 1).times do
+            temp = []
+            temp << sn
+            temp << "1"
+
+            # Invert  and reverse Sn
+            wip = []
+            sn.split('').each do |bit|
+                if bit == "0"
+                    wip << "1"
+                else
+                    wip << "0"
+                end
+            end
+            temp << wip.reverse
+            sn = temp.join
+        end
+    end
+    
+    sn.split('')[k -1]
+end
+
+p find_kth_bit( 3, 1) #"0"
+p find_kth_bit( 4, 11) #"1"
